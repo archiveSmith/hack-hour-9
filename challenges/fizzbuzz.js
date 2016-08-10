0960c91b52a00f95be7fbec9613bec282c1d8982
jaycetan <jaycetan@gmail.com>
2016-08-10 10:11:09 -0700

    final update

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -18,35 +18,20 @@
 
 
 function fizzbuzz(num) {
-    //create array.
-    //count numbers to num. 
-    //if number is divisble by 3.
-    //replace number with fizz.
-    //if number is divisble by 5. 
-    //replace number with buzz. 
-    //if number is divisble by 3 and 5.
-    //replace with fizzbuzz. 
-
     let arr = [];
-
     for (var i = 1; i < num; i++) {
         if (i % 3 === 0 && i % 5 !== 0) {
             arr.push('fizz');
         } else if (i % 5 === 0 && i % 3 !== 0) {
-            arr.push('fizz');
+            arr.push('buzz');
         } else if (i % 3 === 0 && i % 5 === 0) {
-            arr.push('fizz');
+            arr.push('fizzbuzz');
         } else {
             arr.push(i);
         }
     }
-
-    console.log(arr);
-
-
     return arr;
 }
 
-fizzbuzz(16);
 
 module.exports = fizzbuzz;
