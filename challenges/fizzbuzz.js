b2b0b1396698673b7cc49058abaad174dcc15e3a
David Petri <petridw@gmail.com>
2016-08-10 09:09:18 -0700

    Hack-hour-gui creating challenges/fizzbuzz.js

---
+++ challenges/fizzbuzz.js
@@ -0,0 +1,23 @@
+// Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
+// fizzbuzz(16);  -> [ 1,
+//                     2,
+//                     'fizz',
+//                     4,
+//                     'buzz',
+//                     'fizz',
+//                     7,
+//                     8,
+//                     'fizz',
+//                     'buzz',
+//                     11,
+//                     'fizz',
+//                     13,
+//                     14,
+//                     'fizzbuzz',
+//                     16 ]
+
+function fizzbuzz(num) {
+
+}
+
+module.exports = fizzbuzz;
