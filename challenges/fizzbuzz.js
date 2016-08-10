9792e1555833d926a4dad069528d1b9d055100ce
bryanyee <yee.bryan.c@gmail.com>
2016-08-10 10:06:40 -0700

    I fizzed this one

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,15 @@
 //                     16 ]
 
 function fizzbuzz(num) {
+  let newArr = [];
+  for (let i = 0; i <= num; i++) {
+    if (num % 3 === 0 && num % 5 === 0) { newArr.push('fizzbuzz') }
+    else if (num % 3 === 0) { newArr.push('fizz') }
+    else if (num % 5 === 0) { newArr.push('buzz') }
+    else { newArr.push(i) }
+  }
 
+  return newArr;
 }
 
 module.exports = fizzbuzz;
