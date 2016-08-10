e3933cc5c47ad60dca276ea0de53790403cb9132
dianacheung <diana.cheung.2013@gmail.com>
2016-08-10 10:05:28 -0700

    finished fizzbuzz

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,38 @@
 //                     16 ]
 
 function fizzbuzz(num) {
+	// set constants
+	const fizzStr = 'fizz';
+	const buzzStr = 'buzz';
+	const fizzbuzzStr = 'fizzbuzz';
+	
+	// create output [] with size of input num
+	var output = [];
+	//console.log(output.length);
+	
+	// iterate through output []
+	for(var i = 1; i <= num; i++) {
+		// check if divisible by 3 (%3 === 0) & 5 (% 5 === 0)
+		if( (i % 3 === 0) && (i % 5 === 0) ) {
+			// insert "fizzbuzz"
+			output.push(fizzbuzzStr);
+		} else if(i % 3 === 0) { // else if divisble by 3 only
+			// insert "fizz"
+			output.push(fizzStr);
+		} else if (i % 5 === 0) { // else if divisible by 5 only
+			// insert "buzz"
+			output.push(buzzStr);
+		} else {
+			output.push(i);
+		}		
+	}
 
+	// return output []
+	return output;
 }
 
+//console.log(fizzbuzz(16));
+
 module.exports = fizzbuzz;
+
+
