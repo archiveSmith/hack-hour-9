97db808e87a8fdabcc6f778aca85f16a03b3fb7e
dianacheung <diana.cheung.2013@gmail.com>
2016-08-12 09:58:05 -0700

    finished draw stairs

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,35 @@
 */
 
 function drawStairs(n) {
-
+  const asterisks = '*';
+  const whitespace = ' ';
+  
+  // check for n out of bounds
+  if(n < 1 || n > 100) {
+	  return false;
+  }
+  
+  // iterate from 1 to n inclusive
+  for(var i = 1; i <= n; i++) {
+	  var numSpaces = n - i; // calculate # of spaces needed on left
+	  var numAsterisks = n  - numSpaces; // calculate # of asterisks needed on right
+	  
+	  // use String built in repeat function for building repeated spaces and asterisks
+	  var output = whitespace.repeat(numSpaces) + asterisks.repeat(numAsterisks); 
+	  
+	  console.log(output);
+	  
+  }
+  
+  return true;
+    
 }
 
+/* test cases */
+//drawStairs(6);
+//drawStairs(-2);
+//drawStairs(100);
+//drawStairs(1);
+//drawStairs(101);
 
 module.exports = drawStairs;
