480e8bac7137797dca25c8356ed10c7e260757f6
dianacheung <diana.cheung.2013@gmail.com>
2016-08-13 09:59:53 -0700

    finished mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,46 @@
 
 
 function modemean(array) {
+	
+	// mode is max frequency, use map to remember num of times seen this particular number
+	var frequency = {};
+	// keep track of highest frequency seen at this point
+	var mode = 0;
+	
+	// mean is average = sum / num of elements
+	var sum = 0;
+	var numOfElements = array.length;
+	
+	// iterate through array to get accumulated sum, update map, and track mode
+	for(var i = 0; i < array.length; i++) {
+		sum += array[i];
+		
+		var val;
+		
+		if(frequency[array[i]] !== undefined) {
+			// is in map, increment the value
+			val = Number.parseInt(frequency[array[i]], 10);			
+			frequency[array[i]] = ++val;
+		} else { // not in map, add it with 1
+			val = 1;
+			frequency[array[i]] = val;
+		}
+		
+		// update mode as needed
+		if(val > mode) {
+			mode = val;
+		}
+		
+	}
+	
+	var mean = sum / numOfElements;
+	
+	return mode === mean? true: false;
 
 }
+
+//console.log(modemean([2,5,5,1,4,7])); // false
+//console.log(modemean([2,2,1,1,3,3])); // true
+//console.log(modemean([2,2,0,1,4,3])); // true
 
 module.exports = modemean;
