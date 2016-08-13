477afe5f78fcb2cf4eb196ae3f61d64697901be6
Jose Neves <joc.neves@gmail.com>
2016-08-13 14:09:06 -0300

    mode mean done

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,44 @@
 
 
 function modemean(array) {
+  let sortedArr = array.sort((a,b) => {return a-b;});
+  let half = Math.floor(sortedArr.length/2);
+  let mean = 0;
+  let mode = 0;
+  let modeTracking = {};
+  let max = 0;
+  let min = 0;
 
+  console.log(half);
+  if (sortedArr.length % 2) {
+    mean = sortedArr[half];
+  } else {
+    mean = (sortedArr[half-1] + sortedArr[half]) / 2.0;
+  }
+
+  sortedArr.forEach(function(item) {
+    if (!modeTracking[item]){
+      modeTracking[item] = 0;
+      modeTracking[item] += 1;
+      console.log(modeTracking);
+    } else {
+      modeTracking[item] += 1;
+    }
+  });
+
+  for( let num in modeTracking) {
+    if( modeTracking[num] < min) min = modeTracking[num];
+    if( modeTracking[num] > max) max = modeTracking[num];
+  }
+  mode = max + 1;
+  console.log(min,max);
+  console.log(mode,mean);
+
+  if (mean === mode) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = modemean;
