73f1d5afa922c65a58c1b2760f87a34a6c160d0b
waggertron <weylinwagnon@gmail.com>
2016-08-13 10:03:11 -0700

    finished modemean with edgecases" "

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,49 @@
 
 
 function modemean(array) {
+  if (!array.every((item) => typeof item === 'number') && array.length) {
+    console.log("please provide an array of numbers ")
+    return false;
+  }
+  const mode = Math.max(...getMode(array));
+  const mean = getMean(array);
+  return mode === mean;
 
+
+
+
+
+
+  function getMode(arr) {
+
+    let max = 0;
+    let freqTable = arr.reduce((memo, item) => {
+      if (memo[item]) {
+        memo[item]++;
+        if (memo[item] > max) {
+          max = memo[item];
+        }
+      } else {
+        memo[item] = 1;
+        if (max === 0) {
+          max = 1;
+        }
+      }
+      return memo;
+    }, {});
+
+    var mode = [];
+    Object.keys(freqTable).forEach(key => {
+      if (freqTable[key] === max) {
+        mode.push(key);
+      }
+    });
+    return mode;
+  }
+
+  function getMean(arr) {
+    return Math.floor(arr.reduce((memo, item) => memo + item) / arr.length);
+  }
 }
 
 module.exports = modemean;
