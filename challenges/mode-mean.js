5488d1e102522f42cbe11d02e3deb95c67ca011c
jwookie55 <jwookie55@live.com>
2016-08-13 09:59:29 -0700

    finished mode/mean with a refactor for big O time

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,89 @@
 
 
 function modemean(array) {
+	
+	function mean(array){
+		let output = 0;
+		array.forEach(function(num){
+			output += num;
+		});
+		output = output / array.length;
+		console.log('mean', output)
+		return output;
+	}
 
+		// function mode(array){
+		// 	let mode = 0, modeCount = 0;
+		// Refactored below for better O time.
+		// array.forEach(function(num, index){
+		// 	let count = 0;
+		// 	for(let i = index; i < array.length; i++){
+		// 		if(array[i] === num) count++;
+		// 	}
+		// 	if(count > modeCount){
+		// 		mode = num;
+		// 		modeCount = count;
+		// 	}else if(count === modeCount && num > mode){
+		// 		mode = num;
+		// 	}
+		// });
+		function mode(array){
+			let modeObj = {};
+			for(let i = 0; i < array.length; i++){
+				if(modeObj[array[i]]){
+					modeObj[array[i]] = modeObj[array[i]] + 1;
+				}else{
+					modeObj[array[i]] = 1;
+				}
+			}
+			let outMode = 0, outModeCount = 0;
+			for(let key in modeObj){
+				if(modeObj[key] > outModeCount){
+					outMode = key;
+					outModeCount = modeObj[key];
+				}else if(modeObj[key] = outModeCount && key > outMode){
+					outMode = key;
+				}
+			}
+			console.log('mode', outMode)
+			console.log('obj', modeObj)
+			return Number(outMode);
+		}
+
+	let meanOut = mean(array);
+	let modeOut = mode(array);
+	if(meanOut === modeOut){
+		return true;
+	}else{
+		return false;
+	}
 }
 
+
+modemean([2,1])
+
 module.exports = modemean;
+
+
+	function mode(array){
+		let modeObj = {};
+		for(let i = 0; i < array.length; i++){
+			if(modeObj[i]){
+				modeObj[i] = modeObj[i] + 1;
+			}else{
+				modeObj[i] = 1;
+			}
+		}
+		let outMode = 0, outModeCount = 0;
+		for(let key in modeObj){
+			if(modeObj[key] > outModeCount){
+				outMode = key;
+			}else if(modeObj[key] = outModeCount && key > outMode){
+				outMode = key;
+			}
+		}
+		console.log('mode', outMode)
+		return Number(outMode);
+	}
+
+	mode([1,1,2])
