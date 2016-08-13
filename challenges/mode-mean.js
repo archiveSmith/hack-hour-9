0c071895e6891bef9b4785b85076ea0370de575c
Dana Tran <dana.l.tran@gmail.com>
2016-08-13 09:59:08 -0700

    completed mode-mean hack hour

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,36 @@
  *
  */
 
-
 function modemean(array) {
+	let sum = array.reduce(function(prev, curr){
+		return prev + curr;
+	});
+	let mean = Math.floor(sum / array.length);
+
+	//get mean: assign quantity of each number as a value to its key
+	let obj = {};
+	array.forEach(function(num){
+		!obj[num] ? obj[num] = 1 : obj[num] += 1;
+	});
+
+	let mode = 0;
+	let quantity = 0;
+	for (var key in obj) {
+		if (obj[key] > quantity) {
+			quantity = obj[key];
+			mode = key;
+		}
+		if (obj[key] === quantity) {
+			if (key > mode) {
+				quantity = obj[key];
+				mode = key;
+			}
+		}
+	}
+
+	//== operator for type conversion. compare mode and mean values.
+	return mode == mean ? true: false;
 
 }
-
+console.log(modemean([3, 2, 1]));
 module.exports = modemean;
