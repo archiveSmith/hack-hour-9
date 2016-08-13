5c00c208453e580c3fc847526b67c8ef870a0e51
Simon Situ <ssitu001@gmail.com>
2016-08-13 09:40:27 -0700

    complete mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -12,6 +12,41 @@
 
 function modemean(array) {
 
+  let storage = {};
+  let mean = Math.round(array.reduce( (a, b) => a + b) / array.length);
+
+  let mode = 0; 
+  let largestMode = 0;
+  let times = 0;
+
+  array.forEach( (item) => {
+    if(!storage[item]) {
+      storage[item] = 1;
+    } else {
+      storage[item]++;
+    }
+
+  });
+
+
+  for (let key in storage) {
+    if(storage[key] >= times ) {
+    times = storage[key];
+    largestMode = key;
+    mode = Math.max(largestMode, key);
+    }
+  }
+  
+  //console.log("mean", mean)
+  //console.log("mode", mode);
+  
+  //console.log(storage)
+  
+  return Number(mode) === mean;
+
+
 }
+
+console.log(modemean([4,4, 1,1,1,1]));
 
 module.exports = modemean;
