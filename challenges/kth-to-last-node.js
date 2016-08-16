a64fb4ad365c4540c68321d773389b3f1ba02f55
Daniel King <daniel.oliver.king@gmail.com>
2016-08-16 10:02:54 -0700

    Added kth to last

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -16,12 +16,47 @@
  * kthToLastNode(2,a); -> returns the node with the value 'D' (the second to last node)
  */
 
+// Return undefined if k is not a number
+// Return undefined if k is not an integer
+// Return undefined if k is not positive
+// Return undefined if the list is shorter than length k
+
 function Node(val) {
   this.value = val;
   this.next = null;
 }
 
 function kthToLastNode(k, head) {
+  if (typeof k !== 'number' ||
+      !Number.isInteger(k)  ||
+      k < 1) {
+    return undefined;
+  }
+
+  var follower = null;
+  let count = 0;
+  for (let currentNode = head; currentNode; currentNode = currentNode.next) {
+    if (count === k - 1) {
+      follower = head;
+      console.log("Follower: " + follower.value);
+    } else {
+      count++;
+    }
+
+    console.log("Leader " + currentNode.value);
+    console.log("Follower " + follower.value);
+
+    if (follower !== null) {
+      follower = follower.next;
+    }
+  }
+
+  if (follower !== null) {
+    return follower.value;
+  } else {
+    return undefined;
+  }
+
 
 }
 
