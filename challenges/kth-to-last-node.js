993930b0b855104da57cb324131df9cc600cc3c9
dianacheung <diana.cheung.2013@gmail.com>
2016-08-16 09:54:21 -0700

    finished kthToLastNode

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,59 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+	
+	// result - inially undefined
+	var result;
+	
+	// execute recursive call
+	find(k, head);
+	
+	return result;
+	
+	
+	// inner motor function
+	function find(k, currNode) {
+		var index;
+		
+		// base case that we are the last one, so we are 1 from the end
+		if(currNode.next === null) {
+			index = 1;
+		} else {
+			// non base case - find out how far we are from the end
+			index = 1 + find(k, currNode.next);
+		}
+
+		if(index === k) { // we are here!!!
+			// set result
+			result = currNode.value;
+		}
+		
+		return index;		
+	}
 
 }
+
+// test case
+/*
+var a = new Node('A');
+var b = new Node('B');
+var c = new Node('C');
+var d = new Node('D');
+var e = new Node('E');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+console.log(kthToLastNode(1,a));
+console.log(kthToLastNode(2,a));
+console.log(kthToLastNode(3,a));
+console.log(kthToLastNode(4,a));
+console.log(kthToLastNode(5,a));
+console.log(kthToLastNode(6,a));
+console.log(kthToLastNode(0,a));
+console.log(kthToLastNode(-1,a));
+*/
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
