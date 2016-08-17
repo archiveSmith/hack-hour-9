e00776f8c584d18b99f1bbcdaa2954778d267c44
dianacheung <diana.cheung.2013@gmail.com>
2016-08-17 09:39:41 -0700

    finished pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,40 @@
  */
 
 function pow(base, power) {
+	
+	// base case - anything number raised to power of 0 is 1
+	if(power === 0) {
+		return 1;
+	}
+	
+	// check if power is < 0 then it is equal to 1 / (base ^ power)
+	if(power < 0) {
+		return 1 / (pow(base, -1 * power));
+	}
+	
+	// non base case - base number times result of pow() passing in power - 1
+	return base * pow(base, power - 1);
 
 }
+
+/* test cases */
+/*
+console.log(pow(2,0));
+console.log(pow(2,1));
+console.log(pow(2,2));
+console.log(pow(2,3));
+
+console.log(pow(2,-1));
+console.log(pow(2,-2));
+
+console.log(pow(10,0));
+console.log(pow(10,1));
+console.log(pow(10,2));
+console.log(pow(10,3));
+
+console.log(pow(10,-1));
+console.log(pow(10,-2));
+console.log(pow(10,-3));
+*/
 
 module.exports = pow;
