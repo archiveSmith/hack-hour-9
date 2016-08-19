44571d36de87a9bd3485b8c9d8ac3127caee45f5
William Barnes <barnesw91291@gmail.com>
2016-08-19 09:25:26 -0700

    reverse array in place

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -12,6 +12,13 @@
  */
 
 function reverseInPlace(array) {
+	for(let i = 0; i < Math.floor(array.length/2); i++) {
+		temp = array[i];
+		array[i] = array[array.length-i-1];
+		array[array.length-i-1] = temp;
+	}
+
+	return array;
 
 }
 
