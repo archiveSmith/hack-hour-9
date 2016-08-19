dd5670f2209e9254a2d160983eb14351e0b16391
jafow <jared.a.fowler@gmail.com>
2016-08-18 17:28:42 -0700

    pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,11 @@
  * Use recursion!
  */
 
-function pow(base, power) {
-
+function pow(base, power, res = 1) {
+  if (power == 0) return res;
+  else {
+    return pow(base, power - 1, res * base)
+  }
 }
 
 module.exports = pow;
