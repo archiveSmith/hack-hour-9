6b4f7e2f8301a01464644f489511b3303766628c
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-08-20 09:59:10 -0700

    finished string rotation

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -11,12 +11,21 @@
  *              stringRotation("hello", "ollhe") -> false (not a rotation, just an anagram)
  */
 
-function isSubstring(s1, s2) {
-  return s1.indexOf(s2) >= 0;
-}
+ function isSubstring(s1, s2) {
+   return s1.indexOf(s2) >= 0;
+ }
 
-function stringRotation(s1, s2) {
+ function stringRotation(s1, s2) {
+ 	if(s1.length !== s2.length){
+ 		return false;
+ 	}
+ 	let sameChar =  s1.split('').sort().join('') === s2.split('').sort().join('');
+ 	if(!sameChar){
+ 		return false;
+ 	}
+ 	return isSubstring(s1, s2);
 
-}
+ }
+
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
