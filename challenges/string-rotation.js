99192bc108b59763e64e929ab50e0944897cea69
shawn-southwell <shawn.southwel@gmail.com>
2016-08-20 09:45:21 -0700

    different solution

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -17,9 +17,10 @@ function isSubstring(s1, s2) {
 
 function stringRotation(s1, s2) {
 if (s1.length !== s2.length) return false;
-let s = s1[0], startIndex = s2.indexOf(s), start = s2.slice(startIndex);
-return start+s2.slice(0,startIndex) === s1;
-
+// let s = s1[0], startIndex = s2.indexOf(s), start = s2.slice(startIndex);
+// return start+s2.slice(0,startIndex) === s1;
+let doub = s2 + s2;
+return doub.indexOf(s1)  > -1;
 }
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
