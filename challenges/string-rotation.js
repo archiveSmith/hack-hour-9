226426f36131be22fc3acd8762c33e5873806234
Stephen Langdon <stphnlngdncoding@gmail.com>
2016-08-20 09:34:25 -0700

    refactored solution

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -16,13 +16,8 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
-	if (!s2) return false;
-	let iFirstLetter = s2.indexOf("h")
-	if (iFirstLetter === -1) return false;
-	let firstPart = s2.slice(iFirstLetter);
-	let secondPart = s2.slice(0, iFirstLetter)
-	let secondProper = firstPart + secondPart;
-	return isSubstring(s1, secondProper);
+	if (s1.length !== s2.length) return false;
+	return isSubstring(s2 + s2, s1);
 }
 
 
