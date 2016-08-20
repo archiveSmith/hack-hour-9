817d81448a76a9ce6cb9f679e9a0be9c78317873
sneakykate <sneakyk8@gmail.com>
2016-08-20 10:00:20 -0700

    bad approach

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -16,7 +16,49 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
-
+	const length = s1.length;
+	if (s2.length !== length){
+		return false;
+	}
+	else{
+		let s1array = s1.split("");
+		let s2array = s2.split("");
+		const firstCharMatch = s1array.indexOf(s2array[0]);
+		
+		let endmatch;
+		let startmatch;
+		for(let i = firstCharMatch, j = 0; i < length; i++, j++  ){ 
+			if (s1array[i] !== s2array[j]){
+				endmatch = false;
+			
+			}
+			else{
+				endmatch = true;
+				console.log("endmatch is " + endmatch);
+			}
+		for (let k = 0, l = (length - firstCharMatch); l < length; k++, l++){
+			console.log(s1array[k]);
+			console.log(firstCharMatch);
+			if (s1array[k] !== s2array[l]){
+				startmatch = false;
+			}
+			else{
+				startmatch = true;
+			}
+		}
+		// console.log("endmatch is " + endmatch);
+		console.log("startmatch is " + startmatch);
+		}
+		if (startmatch && endmatch){
+			return true;
+		}
+		else{return false;}
+	}
 }
+
+console.log(stringRotation("october", "oberoct"))
+//length has to match
+//approach 1: figure out index of first character to match, loop through end and then loop through beginning to see if still match; then figure out if another index is starting point... this solution went nowhere.
+//approach 2: figure out if there's a shorter way to do this.
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
