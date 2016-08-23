43877c87d44b9cf2c462193e25bf0dce5f95fed6
dianacheung <diana.cheung.2013@gmail.com>
2016-08-23 09:46:25 -0700

    finished balanced parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,62 @@
  */
 
 function balancedParens(input){
+	var bStack = []; // keep track of open bracket encounters
+	
+	for(var index = 0; index < input.length; index++) {
+		var ch = input.charAt(index);
+		
+		// check the char at index, if open bracket type, push on stack, 
+		// if close bracket type that matches topmost char on stack, pop off stack
+		// else return false
+		switch(ch) {
+			case '(':
+			    bStack.push(ch);
+				break;
+			case ')':
+				if(bStack.pop() !== '(') { // unbalanced
+					return false;
+				}
+				break;
+			case '[':
+			    bStack.push(ch);
+				break;
+			case ']':
+				if(bStack.pop() !== '[') { // unbalanced
+					return false;
+				}
+				break;
+			case '{':
+			    bStack.push(ch);
+				break;
+			case '}':
+				if(bStack.pop() !== '{') { // unbalanced
+					return false;
+				}
+				break;
+		}		
+	}
+	
+	return (bStack.length > 0)? false : true;
 
 }
+/* tests */
+/*
+console.log(balancedParens('('));  // false
+console.log(balancedParens('()')); // true
+console.log(balancedParens(')('));  // false
+console.log(balancedParens('(())'));  // true
+
+console.log('-----');
+
+console.log(balancedParens('[](){}')); // true
+console.log(balancedParens('[({})]'));   // true
+console.log(balancedParens('[(]{)}')); // false
+
+console.log('-----');
+
+console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }')); // true
+console.log(balancedParens(' var hubble = function() { telescopes.awesome();')); // false
+*/
 
 module.exports = balancedParens;
