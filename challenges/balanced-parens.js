069f66100112f775d6c3b68449e879614efb84bd
William Barnes <barnesw91291@gmail.com>
2016-08-23 10:01:44 -0700

    for whoever has to read this, i apologize... has to be a better way than this...

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,6 +25,46 @@
  */
 
 function balancedParens(input){
+	//getting all parens
+	let allParens = [];
+	let allBrackets = [];
+	let allMustaches = [];
+	let allChars = [];
+	input = input.split("");
+	for (let i = 0; i < input.length; i++) {
+		if (input[i] === '(' || input[i] === ')') {
+			allParens.push(input[i]);
+			allChars.push(input[i]);
+		} else if (input[i] === '{' || input[i] === '}') {
+			allMustaches.push(input[i]);
+			allChars.push(input[i]);
+		} else if (input[i] === '[' || input[i] === ']') {
+			allBrackets.push(input[i]);
+			allChars.push(input[i]);
+		}
+	}
+	
+	if (allParens.length === 1) return false;
+	if (allBrackets.length === 1) return false;
+	if (allMustaches.length === 1) return false;
+	
+	for (let i = 0; i < Math.floor(allParens.length/2); i++) {
+		if (allParens[allParens.length - i - 1] !== ')'){
+			return false;
+		}
+	}
+	for (let i = 0; i < Math.floor(allBrackets.length/2); i++) {
+		if (allBrackets[allBrackets.length - i - 1] !== ']'){
+			return false;
+		}
+	}
+	for (let i = 0; i < Math.floor(allMustaches.length/2); i++) {
+		if (allMustaches[allMustaches.length - i - 1] !== '}'){
+			return false;
+		}
+	}
+	
+	return true;
 
 }
 
