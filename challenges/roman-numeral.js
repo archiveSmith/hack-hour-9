1904c71ee44b19f0be0052a582098382ea348ae4
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-08-24 10:00:51 -0700

    add roman num

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,24 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
-function romanNumeral(n) {
+ function romanNumeral(n) {
+ 	var roman = "";
+ 	var numArr = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
+ 	var letters = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
+ 	var current;
+ 	console.log(numArr);
+ 	for (var k = numArr.length -1 ; k >= 0; k--){
+ 		current = Math.floor(n/numArr[k]);
+ 		for (var i = 0; i < current; i++){
+ 			roman += letter[k];
+ 		}
+ 		n = n - current * numArr[k];
+ 	}
+ 	return roman;
+ }
 
-}
 
 module.exports = romanNumeral;
