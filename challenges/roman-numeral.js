4ce7c05a7b7fb0f9998eb1d2bbfd0ca0cf6f8348
Simon Situ <ssitu001@gmail.com>
2016-08-24 09:55:29 -0700

    complete romannumeral

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,35 @@
  */
 
 function romanNumeral(n) {
+  let result = '';
+
+    let romans = {
+      "M": 1000,
+      "CM": 900,
+      "D": 500,
+      "CD": 400,
+      "C": 100,
+      "XC": 90,
+      "L": 50,
+      "XL": 40,
+      "X": 10,
+      "IX": 9,
+      "V": 5,
+      "IV": 4,
+      "I": 1
+  };
+
+  for (let key in romans) {
+    while(n >= romans[key]) {
+      result += key;
+      n -= romans[key];
+    }
+  }
+
+  return result;
 
 }
+
+console.log(romanNumeral(560));
 
 module.exports = romanNumeral;
