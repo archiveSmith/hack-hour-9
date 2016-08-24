9520278d8aed2691b986e0c47621030e1480869c
Mike McCready <mikem@inspero.net>
2016-08-24 09:59:39 -0700

    initial solution

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -17,8 +17,37 @@
  * 
  */
 
-function romanNumeral(n) {
+const numValues = {
+	'M': 1000,
+	'CM': 900,
+	'D': 500,
+	'CD': 400,
+	'C': 100,
+	'XC': 90,
+	'L': 50,
+	'XL': 40,
+	'X': 10,
+	'IX': 9,
+	'V': 5,
+	'IV': 4,
+	'I': 1
+}
 
+const romanNumeral = (n) => {
+	let val = n;
+	let res = '';
+	const checkVal = () => {
+		if(val === 0) return res;
+		for(let numeral in numValues) {
+			if(val >= numValues[numeral]) {
+				res += numeral;
+				val -= numValues[numeral];
+				checkVal(val, res)
+			}
+		}	
+	}
+	console.log(checkVal());
+	return checkVal();
 }
 
 module.exports = romanNumeral;
