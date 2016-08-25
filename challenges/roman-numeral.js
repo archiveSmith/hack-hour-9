035dcb698a27ea6d7d7c6373e2f2deffc5f4eb0d
shawn-southwell <shawn.southwel@gmail.com>
2016-08-24 21:30:17 -0700

    finished roman-numeral

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -19,6 +19,26 @@
 
 function romanNumeral(n) {
 
+
+  let numVal = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+  let romanNum = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+
+  var convStr = '';
+  // iterate over numerical equivalents to roman numbers
+  // if n is 543, for instance, we would get to 500, add the corresponding
+  //romanChar to convStr, subtract 500, check for 43, get to 40, add XL,
+  //subtract 40, now check for 3 and add three Is.  terminate the while loop and return convStr.
+  // (✖﹏✖)
+  for (let i = 0; i < numVal.length; ++i) {
+    while (numVal[i] <= n) {
+      convStr += romanNum[i];
+      n -= numVal[i];
+    }
+  }
+
+  return convStr;
+
 }
 
 module.exports = romanNumeral;
+
