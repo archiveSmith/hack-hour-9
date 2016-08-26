9a79b0ac2e41fae15f66658af70bfa5e9ff0f174
Daniel King <daniel.oliver.king@gmail.com>
2016-08-26 10:01:01 -0700

    Finished subse sum, not very efficient

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -8,8 +8,40 @@
  * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
  */
 
-function subsetSum(array, target) {
+// Idea for improving efficiency: Memoize the function.
+// What do I need to remember in order to avoid duplication
+// of effort?
+// 
+// Another idea: On each time through the for loop, shift off
+// the first element of the array, because if we made it through
+// that iteration, we've already tried every possible subset
+// including that number.
 
+function subsetSum(array, target) {
+  // Argument type checking
+  if (!Array.isArray(array)) return undefined;
+  if (!array.every(item => typeof item === 'number')) return undefined;
+  if (typeof target !== 'number') return undefined;
+  if (Number.isNaN(target)) return undefined;
+  if (!Number.isFinite(target)) return undefined;
+  
+  // Base cases: Either we've run out of numbers in the array,
+  // or we've hit the target
+  if (array.length === 0) return false;
+  if (target === 0) return true;
+  
+  // Recursive case
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    const arrayWithoutElement = Array.from(array)
+    arrayWithoutElement.splice(i, 1);
+
+    if (subsetSum(arrayWithoutElement, target - element)) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 module.exports = subsetSum;
