f6fbf975358ebf3772f412177426ba1ba9729440
Dana Tran <dana.l.tran@gmail.com>
2016-08-27 10:08:29 -0700

    completed bin to dec hack hour

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -15,6 +15,40 @@
 
 function binToDec(binary) {
 
+	var bin = [256, 128, 64, 32, 16, 8, 4, 2, 1];
+	
+	while (binary.length < 9) {
+		binary = '0' + binary;
+	}
+	
+	var sum = 0;
+	
+	for (var i = 0; i < binary.length; i++) {
+		binary[i] === '1' ? sum += bin[i] : sum;
+	}
+	
+	return sum;
+}
+
+function decToBin(decimal) {
+	var bin = [256, 128, 64, 32, 16, 8, 4, 2, 1];
+	
+	while (decimal > bin[0]) {
+		bin.unshift(bin[0] * 2);
+	}
+	
+	var copy = [];
+
+	for (var i = 0; i < bin.length; i++) {
+		if (decimal >= bin[i] && decimal > 0) {
+			copy.push('1');
+			decimal -= bin[i];
+		} else {
+			copy.push('0');
+		}
+
+	}
+	return copy.join('');
 }
 
 module.exports = binToDec;
