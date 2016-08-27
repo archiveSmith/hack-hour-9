832e6fca3444ef2db1603fbafcff80dd1a4036da
Todd Chisom <hello@toddchisom.me>
2016-08-27 09:36:13 -0700

    binary to int complete with Weylin

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -1,12 +1,13 @@
+
 /* You are given a string that represents a binary number
  *
  * Write a function that converts the binary string to a decimal number
  *
  * Example:
-* 	binToDec('0')   -> 0
- * 	binToDec('11')  -> 3
- * 	binToDec('100') -> 4
- * 	binToDec('101') -> 5
+*   binToDec('0')   -> 0
+ *  binToDec('11')  -> 3
+ *  binToDec('100') -> 4
+ *  binToDec('101') -> 5
  *  binToDec('0101') -> 5
  *
  * Extension:
@@ -14,7 +15,10 @@
  */
 
 function binToDec(binary) {
-
+    let num = 0;
+    for (let i = 0, len = binary.length; i < len; i++) {
+       num += parseInt(binary[i]) * Math.pow(2,(len - 1) - i)
+    }
+    return num;
 }
-
 module.exports = binToDec;
