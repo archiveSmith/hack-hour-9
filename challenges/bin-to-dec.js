d8d11c5abb6794619a9acf30b643fa2d87e8d806
codesmith <student@codeswiki.local>
2016-08-27 09:49:20 -0700

    finished bin to dec

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,17 @@
  */
 
 function binToDec(binary) {
+ 	var sum = 0;
+ 	const BASE = 2;
 
+ 	var myStrArray = binary.split('').reverse();
+
+ 	for(var counter = 0; counter < myStrArray.length; counter++) {
+
+ 		sum += Math.pow(BASE, counter) * parseInt(myStrArray[counter], 10);
+ 	}
+
+ 	return sum;
 }
 
 module.exports = binToDec;
