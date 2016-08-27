36f100e966cd577aef3f3afd379e38ef29078db0
codesmith <student@codeswiki.local>
2016-08-27 09:59:51 -0700

    implement per hack hour solution for subset-sum

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -6,10 +6,27 @@
  * subsetSum([3, 34, 4, 12, 5, 12], 32) -> true, 3 + 12 + 5 + 12 = 32
  * subsetSum([8, 2, 4, 12], 13) -> false
  * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
+ *
+ * extend this to work for subsetProduct
  */
 
 function subsetSum(array, target) {
+	if(target === 0) {
+		return true;
+	}
+
+	if(array.length === 0) {
+		return false;
+	}
+
+	return subsetSum(array.slice(1), target - array[0]) || subsetSum(array.slice(1), target);
 
 }
+
+/* test cases */
+console.log(subsetSum([3, 7, 4, 2], 5)); //- > true, 3 + 2 = 5
+console.log(subsetSum([3, 34, 4, 12, 5, 12], 32)); //-> true, 3 + 12 + 5 + 12 = 32
+console.log(subsetSum([8, 2, 4, 12], 13)); //-> false
+console.log(subsetSum([8, -2, 1, -3], 6)); //-> true, 8 + 1 + (-3) = 6
 
 module.exports = subsetSum;
