66babe2b7e76b18072b34edcfe2e35d20839786c
Todd Chisom <hello@toddchisom.me>
2016-08-30 15:51:20 -0700

    Mactching the words with the balance parens logic

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -3,12 +3,33 @@
 // matchWord('__END_DNE-----');  -> true
 // matchWord('__ENDDNE__');  -> false       (not separated by a space)
 // matchWord('IF()()fi[]');  -> true        (should be case-insensitive)
-// matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ] 
+// matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ]
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
 // matchWord('');  -> true
 
 function matchWord(str) {
+let strArray = str.match(/([a-zA-Z]+)/g);
+
+let testArray = [];
+if (str === '') return true;
+
+  for (var i = 0; i < strArray.length; i++) {
+    let revStr = strArray[i].split('').reverse().join('');
+    if(revStr === testArray[testArray.length - 1]) {
+      testArray.pop();
+    } else {
+      testArray.push(strArray[i]);
+    }
+    strArray[i]
+  }
+  if (testArray.length > 0) return false;
+
+  return true;
 
 }
+// console.log()
+// console.log(matchWord('__END_DNE-----'));
+// console.log(matchWord('__ENDDNE__'));
+// console.log(matchWord('IF()()fi[]'));
 
 module.exports = matchWord;
