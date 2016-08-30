0b3c29bce45ceda5bf7ea41dbc975b0fed786dff
yantonsoup <ygoldobin@gmail.com>
2016-08-30 10:01:56 -0700

    need to iterate through array of words to determine open closes now

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -1,4 +1,8 @@
-// Some languages have "if" statements that are closed by "fi" instead of curly brackets. Or they close a "case" with "esac", i.e. the same keyword backwards. for this problem we'll check that all words in a string are "closed". Write a function that takes a string and returns true if every word is closed by its backwards counterpart. Words must be separated by space or punctuation.
+// Some languages have "if" statements that are closed by "fi" instead of 
+// curly brackets. Or they close a "case" with "esac", i.e. the same keyword 
+// backwards. for this problem we'll check that all words in a string are "closed". 
+// Write a function that takes a string and returns true if every word is closed
+// by its backwards counterpart. Words must be separated by space or punctuation.
 
 // matchWord('__END_DNE-----');  -> true
 // matchWord('__ENDDNE__');  -> false       (not separated by a space)
@@ -8,7 +12,32 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+	let string = str.toLowerCase();
+	let wordArray = [];
+	let aWord = '';
+	let allLetters = '';
+	
+	for (let index = 0; index < string.length -1; index++){
+		while(isLowerCaseLetter(string[index])){
+			aWord += string[index];
+			index++;
+		}
+		
+		if(aWord !== ''){
+			wordArray.push(aWord);
+		}
+		aWord = '';
+	}
+	console.log(wordArray);
+	if(wordArray.length === 1){
+		return false;
+	}
 
+	
+}
+
+function isLowerCaseLetter(str) {
+    return (/[a-z]/.test(str));
 }
 
 module.exports = matchWord;
