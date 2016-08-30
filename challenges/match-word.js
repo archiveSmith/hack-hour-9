c022082bc8c2e464a0b53463d09b3d8c33eee909
dianacheung <diana.cheung.2013@gmail.com>
2016-08-30 10:01:46 -0700

    finished match-word

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -8,7 +8,54 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
-
+	// split the string into an array by separator white space and punctuation
+		// replace all non alphanumeric chars with space
+		// then split on all space char
+	const punctuation = /[!"\#$%&'()*+,\-./:;<=>?@\[\\\]^_`{|}~]/g;
+	var newStr = str.replace(punctuation, ' ');
+	newStr = newStr.toLowerCase(); // translate element to lowercase	
+	var strArray = newStr.split(/\s/);
+	
+	// stack to keep track of open statements
+	var myStack = [];
+	
+	// iterate through array of words
+	for(var i = 0; i < strArray.length; i++) {
+		var word = strArray[i];
+		
+		if(word === '') { // empty word, just continue
+			continue;
+		} else {
+			// if stack empty then just push onto stack
+			if(myStack.length === 0) {
+				myStack.push(word);
+			} else { // if stack not empty, pop top element and compare with current element in reverse
+				var topWord = myStack.pop();
+				var currWordReversed = word.split('').reverse().join('');
+				
+				//no match, push popped element back on stack, push current element on stack, else continue
+				if(topWord !== currWordReversed) {
+					myStack.push(topWord);
+					myStack.push(word);
+				}
+			}
+		}
+		
+	}
+	
+	// if stack length > 0, return false, else true
+	return myStack.length === 0 ? true : false;
+	
 }
+
+/* tests */
+/*
+console.log(matchWord('__END_DNE-----')); //  -> true
+console.log(matchWord('__ENDDNE__')); // -> false       (not separated by a space)
+console.log(matchWord('IF()()fi[]'));  //-> true        (should be case-insensitive)
+console.log(matchWord('for__if__rof__fi'));  //-> false     not properly closed. like ( [) ] 
+console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));  //-> true
+console.log(matchWord(''));  //-> true
+*/
 
 module.exports = matchWord;
