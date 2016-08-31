5b77abee70debea4fbae0aa338cee04adcccac6f
Simon Situ <ssitu001@gmail.com>
2016-08-31 09:54:59 -0700

    complete highestProduct

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -1,10 +1,15 @@
-/**
- * Given an array of integers, find the highest product you can get from three of the integers.
- */
-
 function highestProduct(array) {
 
+	array.sort( (a, b) => a - b);
+	
+	let option1 = array[0] * array[1] * array[array.length-1];
+	let option2 = array.slice(-3).reduce( (a,b) => a * b);
+
+	return Math.max(option1,option2)
+
 }
+
+highestProduct([10,-9, -9, -11, 10, 5,4,3,2,1, -12]);
 
 
 module.exports = highestProduct;
