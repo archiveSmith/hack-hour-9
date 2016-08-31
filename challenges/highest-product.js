ab43424d823e7ad94c818d863403fd44c3460f77
bryanyee <yee.bryan.c@gmail.com>
2016-08-31 09:41:44 -0700

    Complete highest-product challenge

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,19 @@
  */
 
 function highestProduct(array) {
+  if (!Array.isArray(array) || array.length < 3) return undefined;
 
+  let topIntegers = array.reduce((cache, currentInt) => {
+    cache.push(currentInt);
+    if (cache.length > 3) {
+      let smallest = Math.min(...cache);
+      let index = cache.indexOf(smallest);
+      cache.splice(index, 1);
+    }
+    return cache;
+  }, []);
+
+  return topIntegers[0] * topIntegers[1] * topIntegers[2];
 }
-
 
 module.exports = highestProduct;
