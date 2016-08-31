09ed48ec6804dd017e1f3a5271ca7c7ce4369915
shawn-southwell <shawn.southwel@gmail.com>
2016-08-31 09:35:55 -0700

    finished highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,15 @@
  */
 
 function highestProduct(array) {
-
+if (array.length < 1) {return undefined;} 
+const sorted = array.sort((a,b) => b - a ),
+max = (array[0] * array[1] * array[2]),
+min =  (array[0] * array[array.length-1] * array[array.length-2]);
+if (Math.max(...array) < 0){
+	const rev = sorted.reverse();
+	return max;
 }
-
+return Math.max(...[max,min]);
+}
 
 module.exports = highestProduct;
