4514d07b471568d87664534e47b1cfde6685a2ef
Daniel King <daniel.oliver.king@gmail.com>
2016-08-31 09:50:12 -0700

    Refactored to be more transparent

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -5,16 +5,18 @@
 function highestProduct(array) {
   if (array.length < 3) return 0;
 
-  function descending(a, b) {
-    if (a > b) return -1;
-    else if (a < b) return 1;
-    else return 0;
-  }
-
-  function multiply(a, b) {return a * b};
-
   return array.sort(descending).slice(0, 3).reduce(multiply);
 }
+
+function descending(a, b) {
+  if (a > b) return -1;
+  else if (a < b) return 1;
+  else return 0;
+}
+
+function multiply(a, b) {
+  return a * b
+};
 
 
 module.exports = highestProduct;
