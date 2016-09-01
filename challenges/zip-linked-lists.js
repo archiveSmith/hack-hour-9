7ce7b89795f5d2b6589247e046222a841f7cd76c
bryanyee <yee.bryan.c@gmail.com>
2016-09-01 10:01:07 -0700

    partial solution for zip-linked-lists

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,23 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  let c1 = l1;
+  let c2 = l2;
+  let p1 = null;
+  let p2 = null;
+  while (c2) {
+    p1 = c1.next;
+    c1.next = c2;
+    p2 = c2.next;
+    if (p1) c2.next = p1;
+    c1 = p1;
+    if (p2) c1.next = p2;
+    c2 = p2;
+  }
+
 };
 
 module.exports = {Node: Node, zip: zip};
