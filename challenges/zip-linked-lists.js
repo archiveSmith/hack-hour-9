c73d3e4ee8162656ab2096308d0bd561a5e7ebb2
Daniel King <daniel.oliver.king@gmail.com>
2016-09-01 09:57:27 -0700

    Finished zip-linked-lists

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,36 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-};
+  if (l1 === null && l2 === null) return undefined;
+  if (l1 === null) return l2;
+  if (l2 === null) return l1;
+
+  const result = new Node(l1.value);
+  result.next = new Node(l2.value);
+
+  let l1Iterator = l1.next;
+  let l2Iterator = l2.next;
+  let resultIterator = result.next;
+
+  while(l1Iterator && l2Iterator) {
+    resultIterator.next = new Node(l1Iterator.value);
+    resultIterator.next.next = new Node(l2Iterator.value);
+
+    l1Iterator = l1Iterator.next;
+    l2Iterator = l2Iterator.next;
+    resultIterator = resultIterator.next.next;
+  }
+
+  let remainingIterator = l1Iterator ? l1Iterator : l2Iterator;
+
+  while(remainingIterator) {
+    resultIterator.next = new Node(remainingIterator.value);
+
+    remainingIterator = remainingIterator.next;
+    resultIterator = resultIterator.next;
+  }
+
+  return result;
+}
 
 module.exports = {Node: Node, zip: zip};
