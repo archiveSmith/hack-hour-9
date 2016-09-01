8f47130f7a41c9699c9a1c25d1966461196ea563
jwookie55 <jwookie55@live.com>
2016-09-01 14:00:08 -0700

    finished zip with refactor to single for loop

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,40 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+	if(!l1 || !l2){
+		return undefined;
+	}
+
+	let nextNode = l2;
+	for(let i = l1; i; i = i.next.next){
+		let nextI = i.next;
+		i.next = nextNode;
+		nextNode = nextNode.next;
+		i.next.next = nextI;
+	}
+	return l1;
 };
+
+
+// testing code
+// l1 = new Node('l10');
+// l2 = new Node('l20');
+
+// for(let x=1, l=l1, y=l2; x<6; x++, l=l.next, y=y.next){
+// 	l.next = new Node('l1'+x);
+// 	y.next = new Node('l2'+x);
+// }
+
+// let arr = [];
+// for(let i = l1; i.next; i = i.next){
+// 		arr.push(i);
+// 	}
+
+// 	console.log('arr',arr)
+
+// console.log('l1',l1);
+// console.log('l2',l2);
+
+// zip(l1,l2);
 
 module.exports = {Node: Node, zip: zip};
