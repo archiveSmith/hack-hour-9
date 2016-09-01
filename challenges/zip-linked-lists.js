23b9267f4dfc29e29a003359ae315438084ed022
Simon Situ <ssitu001@gmail.com>
2016-09-01 10:11:39 -0700

    complete ziplinkedlist

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,31 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-};
+	
+	if (!l1) return l2;
+  if (!l2) return l1;
+  if (!l1 && !l2) return undefined;
+   	
+	var head = l1.value || l2.value;
 
+   if (l1.value < l2.value) {
+      head = l1;
+      head.next = zip(l1.next, l2);
+   } else {
+      head = l2;
+      head.next = zip(l1, l2.next);
+   }
+   return head;
+}
+
+var list1 = new Node(1);
+list1.next = new Node(3);
+list1.next.next = new Node(5);
+
+var list2 = new Node(2);
+list2.next = new Node(4);
+list2.next.next = new Node(6);
+
+
+zip(list1, list2);
 module.exports = {Node: Node, zip: zip};
