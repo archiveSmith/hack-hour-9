500cab3b61930201b4e607447b41c19786f4f2ff
Dana Tran <dana.l.tran@gmail.com>
2016-08-31 23:56:25 -0700

    completed highest product hack hour

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,19 @@
  */
 
 function highestProduct(array) {
-
+  if (array.length < 3) {
+    return 0;
+  }
+  //sorts array from smallest to largest
+  var sorted = array.sort(function(a, b) {
+    return a - b;
+  });
+  //product of the 2 largest abs values of negatives & last number in sorted array
+  var firstSet = Math.abs(sorted[0]) * Math.abs(sorted[1]) * sorted[sorted.length-1];
+  //product of last 3 numbers in sorted array
+  var lastSet = sorted[sorted.length-1] * sorted[sorted.length-2] * sorted[sorted.length-3];
+  //return larger of the two products
+  return firstSet > lastSet ? firstSet : lastSet;
 }
 
 
