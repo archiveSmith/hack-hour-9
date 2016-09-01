ef94f1a94ea951fe3ad3906a694bf1f63b1f642d
dianacheung <diana.cheung.2013@gmail.com>
2016-09-01 09:23:41 -0700

    finished highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -4,7 +4,38 @@
 
 function highestProduct(array) {
 
+	// edge case - array length less than 3
+	if(array.length < 3) {
+		return 0;
+	}
+	
+	// sort array in ascending order - so array[0] with lowest value
+	var sortedArray = array.sort(function(a, b) {
+		return a - b;
+	});
+	
+    var val1 = productOfThree(sortedArray.slice(-3));
+	var val2 = productOfThree(sortedArray.slice(0, 2).concat(sortedArray.slice(-1)));
+	
+	return Math.max(val1, val2);
+
+	
+	// helper function
+	function productOfThree(arrayOfThree) {
+		var product = arrayOfThree[0];
+		
+		for(var i = 1; i < arrayOfThree.length; i++) {
+			product = product * arrayOfThree[i];
+		}
+		
+		return product;
+	}
 }
 
+/* test cases */
+/*
+console.log(highestProduct([3,5,0,6]));
+console.log(highestProduct([-3,-5,0,2,6]));
+*/
 
 module.exports = highestProduct;
