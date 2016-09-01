5b9c77a4e9289cba8bc80a3939a18f73a53b98f5
jaycetan <jaycetan@gmail.com>
2016-09-01 09:56:51 -0700

    zip LL still in progress.

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,7 +10,36 @@ function Node(val) {
   this.next = null;
 }
 
+//passing in 2 linked lists. 
 function zip(l1, l2) {
+  //declare L1Current and L2Current vars. 
+  //set zippedLL to be L1Current.
+  //update L1Current to be next in line. 
+  //set zippLL next to be L2Current.
+  //update L2Current to be next in line. 
+  let currL1 = l1;
+  let currL2 = l2;
+  let latestZipped;
+  let zipped = l1;
+  
+  //while 
+  while (currL1 !== null ) {
+    
+  } 
+
+  while (currL2 !== null ) {
+    
+  } 
+
+  
+  
+  currL1 = zipped.next;
+  zipped.next = currL2;
+  currL2 = zipped.next;
+
+  
+
+
 };
 
-module.exports = {Node: Node, zip: zip};
+module.exports = { Node: Node, zip: zip };
