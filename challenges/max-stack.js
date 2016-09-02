2dab56bc19bd4c1e226d72b416254003090c842b
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-09-02 09:51:32 -0700

    completed max stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,35 @@
  */
 
 function Stack() {
-  // body...
+  this.storage = {};
+  this.length = 0;
 }
+
+Stack.prototype.push = function(value) {
+	this.storage[this.length] = value;
+	return ++this.length;
+};
+
+Stack.prototype.pop = function() {
+	if(this.length<0){
+		return null	
+	}
+	var locIndex = this.length;
+	this.length--;
+	return this.storage[this.length];
+};
+
+Stack.prototype.max = function() {
+	if(this.length<0){
+		return null	
+	}
+	var maxElement = this.storage[0]
+	for (var i = 0; i < this.length; i++){
+		if (this.storage[i] > maxElement){
+			maxElement = this.storage[i];
+		}
+	}
+	return maxElement
+};
 
 module.exports = Stack;
