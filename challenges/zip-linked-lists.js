eab99ee18d94761e2822d0f4ee4dfa94b1a06740
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-09-01 19:58:09 -0700

    Finished zip

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,16 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-};
+  if (l1 === null) return l2;
+  if (l2 === null) return l1;
+
+  if (l1.value < l2.value) {
+    l1.next = zip(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = zip(l2.next, l1);
+    return l2;
+  }
+}
 
 module.exports = {Node: Node, zip: zip};
