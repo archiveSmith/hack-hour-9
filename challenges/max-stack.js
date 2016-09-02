c46c9e8e0e3c96050cf83d866a5d88d46617ba5d
Simon Situ <ssitu001@gmail.com>
2016-09-02 09:52:28 -0700

    complete Stack using ES6

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -6,8 +6,34 @@
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
 
-function Stack() {
-  // body...
+class Stack {
+	constructor() {
+		this.storage = [];
+		this.greatest = -Infinity;	
+	}
+
+  push(val) {
+    this.greatest = Math.max(this.greatest, val);
+    this.storage[this.storage.length] = val;
+    return this.storage.length;
+  }
+
+  pop() {
+    
+    let popped = this.storage.splice(-1)[0];
+    if(this.greatest === popped) {
+      this.greatest = -Infinity;
+      for (let i = 0; i < this.storage.length; i++) {
+        this.greatest = Math.max(this.greatest, this.storage[i]);
+      }
+    }
+    return popped;
+  }
+
+  getMax() {
+    return this.greatest;	
+  } 
+
 }
 
 module.exports = Stack;
