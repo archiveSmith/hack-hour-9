c9138a5aac39edb852a8bcc17b8504ae567829b4
Mike McCready <mikem@inspero.net>
2016-09-02 09:57:42 -0700

    initial solution maxStack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,28 @@
  */
 
 function Stack() {
-  // body...
+	this.stack = [];
+  this.length = 0;
+	this.push = (num) => {
+		this.stack[stack.length] = num;
+    this.length++;
+		return this.stack.length;
+	}
+	this.pop = () => {
+  	if(this.stack.length === 0) return undefined;
+		let temp = this.stack[this.stack.length - 1]
+		delete this.stack[this.stack.length - 1]
+    this.length--;
+		return temp;
+	}
+  this.getMax = () => {
+  	if(this.stack.length === 0) return undefined;
+  	let tempArray = this.stack.slice();
+    tempArray.sort((a, b) => {
+    	return b - a;
+    });
+    return tempArray[0];
+  }
 }
 
 module.exports = Stack;
