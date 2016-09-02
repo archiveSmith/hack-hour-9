555447fcee02bfd16ddd29c892b3c1ad05864170
Jose Neves <joc.neves@gmail.com>
2016-09-02 13:51:12 -0300

    max stack done

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,30 @@
  */
 
 function Stack() {
-  // body...
+  this.contents = {};
+  this.length = 0;
 }
+
+Stack.prototype.push = function(arr) {
+  this.contents[this.length] = arr;
+  this.length++;
+};
+
+Stack.prototype.pop = function() {
+  let deletedValue = this.contents[this.length-1];
+  delete deletedValue;
+  if (deletedValue !== undefined) this.length--;
+  return deletedValue;
+};
+
+Stack.prototype.getMax = function() {
+  let max = -Infinity;
+  for (let key in this.contents) {
+  	if (this.contents[key] > max) {
+      max = this.contents[key];
+    }
+  }
+  return max;
+};
 
 module.exports = Stack;
