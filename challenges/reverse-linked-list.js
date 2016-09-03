1517d49c1718b113118607b58ae347f9a321b653
Daniel King <daniel.oliver.king@gmail.com>
2016-09-03 09:47:14 -0700

    Iterative solution to reverse-linked-list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,6 +14,42 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
+  // Argument error checking
+  if (head === null) return null;
+  if (head === undefined) return undefined;
+
+  // If the list has length 1, no computation is necessary
+  if (!head.next) return head;
+
+  head.forwardLink = head.next;
+  head.next = null;
+
+  let lead = head.forwardLink;
+  let follow = head;
+
+  // Ierate through nodes, setting 'next' property to
+  // the one behind and following 'forwardLink' forward
+  while(lead) {
+    lead.forwardLink = lead.next;
+    lead.next = follow;
+
+    lead = lead.forwardLink;
+    follow = follow.forwardLink;
+  }
+
+  // Set the new head to be the previous end
+  head = follow;
+
+  // Iterate through the newly reversed list
+  // deleting the 'forwardLink' property
+  while(follow) {
+    delete follow.forwardLink;
+
+    follow = follow.next;
+  }
+
+  return head;
+
 
 }
 
