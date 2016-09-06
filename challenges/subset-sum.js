b984b44d2ff16e4d7a8673dd742d22872ea14569
bryanyee <yee.bryan.c@gmail.com>
2016-09-05 20:53:40 -0700

    Complete subset-sum

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -1,7 +1,7 @@
 /* You are given an array of integers and a target number. Write a function that returns true if
  * there is a subset of the array that sums up to the target and returns false otherwise. A subset
  * can be any size and the elements do not have to appear consecutively in the array.
- * 
+ *
  * subsetSum([3, 7, 4, 2], 5) - > true, 3 + 2 = 5
  * subsetSum([3, 34, 4, 12, 5, 12], 32) -> true, 3 + 12 + 5 + 12 = 32
  * subsetSum([8, 2, 4, 12], 13) -> false
@@ -9,21 +9,26 @@
  */
 
 function subsetSum(array, target) {
-// loop through the array, starting at the first value
-  // loop through the array, and sum the first value with each of the elements
-  // loop again, summing the first two values and summing with each of the other elements
-  // continue adding elements and looping through the array
-// if the first loop didn't pass, pop off the first element and add to the end, then repeat
-  let values = array.slice();
-  for (let i = 0; i < values.length; i++ ) {
-    if (values[0] === target) return true;
-    for (let j = 1; j < values.length) {
-      
-    }
-    
-  }
+  // Base case
+  if (target === 0) return true;
+  if (array.length === 0) return false;
 
-
+  // Recursive case
+  const nextArr = array.slice(1);
+  return subsetSum(nextArr, target - array[0])
+    || subsetSum(nextArr, target);
 }
 
 module.exports = subsetSum;
+
+
+// Pseudocode
+
+// base case:
+  // if target is zero, return true
+  // if array is empty, there's no more possibilities. Return false
+
+// recursive case:
+  // check if we take the first number
+  // check if we don't take the first number
+  // return true  if any of them returns true; return false otherwise
