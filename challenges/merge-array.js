ff14f9886bb07fd20edb563d42ba9a2ab8364472
Jwookie55 <jwookie55@live.com>
2016-09-07 10:11:37 -0700

    recursive version of merge array

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -13,36 +13,37 @@
  *
  */
 
-function mergeArrays(arr1, arr2) {
-  if (arr1.constructor !== Array || arr2.constructor !== Array) {
-    return undefined;
-  }
-  return arr1.concat(arr2).sort((a, b) => a - b);
-}
-
-module.exports = mergeArrays;
+//  brute force
+// function mergeArrays(arr1, arr2) {
+//   if (arr1.constructor !== Array || arr2.constructor !== Array) {
+//     return undefined;
+//   }
+//   return arr1.concat(arr2).sort((a, b) => a - b);
+// }
 
 //  recursive merge
-// function mergeArrays(arr1, arr2, output = []) {
-//   if (arr1.length === 0 && arr2.length === 0) {
-//     return output;
-//   } else if (arr1.length === 0) {
-//     return output.concat(arr2);
-//   } else if (arr2.length === 0) {
-//     return output.concat(arr1);
-//   }
-//   if (arr1[0] < arr2[0]) {
-//     output.push(arr1[0]);
-//     arr1.shift();
-//   } else {
-//     output.push(arr2[0]);
-//     arr2.shift();
-//   }
-//   return mergeArrays(arr1, arr2, output);
-// }
+function mergeArrays(arr1, arr2, output = []) {
+  if (arr1.length === 0 && arr2.length === 0) {
+    return output;
+  } else if (arr1.length === 0) {
+    return output.concat(arr2);
+  } else if (arr2.length === 0) {
+    return output.concat(arr1);
+  }
+  if (arr1[0] < arr2[0]) {
+    output.push(arr1[0]);
+    arr1.shift();
+  } else {
+    output.push(arr2[0]);
+    arr2.shift();
+  }
+  return mergeArrays(arr1, arr2, output);
+}
 
 //  testing data
 // let arr1 = [3, 4, 6, 10, 11, 15, 21];
 // let arr2 = [1, 5, 8, 12, 14, 19];
 
 //  mergeArrays(arr1, arr2);
+
+module.exports = mergeArrays;
