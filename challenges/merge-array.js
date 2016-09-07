c3ec5ad5021e8eb7289bc04f83db6c00712121de
Simon Situ <ssitu001@gmail.com>
2016-09-07 09:56:33 -0700

    complete mergeArrays

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -14,7 +14,32 @@
  */
 
 function mergeArrays(arr1, arr2) {
+	
+	if(!arr1.length) return arr2;
+	if(!arr2.length) return arr1;
+
+	let arr1Index = 0;
+	let arr2Index = 0;
+	
+	let mergedArr = [];
+	let length =  arr1.length + arr2.length;
+	
+	while (mergedArr.length < length) {
+		if (arr1[arr1Index] > arr2[arr2Index]) {
+			mergedArr.push(arr2[arr2Index++]);
+		} else {
+			mergedArr.push(arr1[arr1Index++]);
+		}
+	}
+	
+	return mergedArr;
+	
 
 }
+
+var my_array = [3,4,6,10,11,15,21];
+var another_array = [1,5,8,12,14,19];
+
+mergeArrays(my_array, another_array);
 
 module.exports = mergeArrays;
