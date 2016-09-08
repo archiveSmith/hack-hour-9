af8ff04945ba0b4f424fd6ec437dfb734c8da0a9
Daniel King <daniel.oliver.king@gmail.com>
2016-09-08 09:42:22 -0700

    Handles numbers less than 100

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -11,9 +11,56 @@
  * numToWords(300525151340440) -> 'ThreeHundredTrillionFiveHundredTwentyFiveBillionOneHundredFiftyOneMillionThreeHundredFortyThousandFourHundredForty'
  * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
  */
+const ONES_PLACE = ['', 'One', 'Two', 'Three', 'Four',
+                    'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+
+const STRANGE_TENS = {
+  10: 'Ten',
+  11: 'Eleven',
+  12: 'Twelve',
+  13: 'Thirteen',
+  14: 'Fourteen',
+  15: 'Fifteen',
+  16: 'Sixteen',
+  17: 'Seventeen',
+  18: 'Eighteen',
+  19: 'Nineteen'
+}
+
+const REGULAR_TENS = {
+  2: 'Twenty',
+  3: 'Thirty',
+  4: 'Forty',
+  5: 'Fifty',
+  6: 'Sixty',
+  7: 'Seventy',
+  8: 'Eighty',
+  9: 'Ninety'
+}
 
 function numToWords(num) {
+  if (num === 0) return 'Zero';
 
+  if (num < 10) {
+    return ONES_PLACE[num];
+  } else if (num < 20) {
+    return STRANGE_TENS[num];
+  }
+
+  const ones_place = num % 10;
+  const tens_place = Math.floor(num / 10);
+
+  return REGULAR_TENS[tens_place] + ONES_PLACE[ones_place];
 }
+
+function getSmallestDigits(numberOfDigits, num) {
+  return num % (Math.pow(10, numberOfDigits - 1));
+}
+
+function truncateLastThreeDigits(num) {
+  return Math.floor(num / 1000);
+}
+
+numToWords.getSmallestDigits = getSmallestDigits;
 
 module.exports = numToWords;
