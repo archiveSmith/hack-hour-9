da25f30efa71612517067c4e21ea8b9b19522656
bryanyee <yee.bryan.c@gmail.com>
2016-09-09 09:42:47 -0700

    Complete perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,22 @@
  */
 
 function permPalin(str) {
-	
+  str = str.toLowerCase();
+  const cache = {};
+  let oddCount = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (cache[char]) cache[char]++;
+    else cache[char] = 1;
+  }
+
+  for (let key in cache) {
+    if (cache[key] % 2 === 1) oddCount++;
+    if (oddCount > 1) return false;
+  }
+
+  return true;
 }
 
 module.exports = permPalin;
