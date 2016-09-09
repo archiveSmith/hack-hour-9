b37c0ee747f4a5b1797102edf2cb504e01bcb267
yantonsoup <ygoldobin@gmail.com>
2016-09-09 10:07:12 -0700

    perm-palin done

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,32 @@
  */
 
 function permPalin(str) {
-	
+	if (str.length === 1) {return true}
+	// console.log(str.length)
+	segments = [];
+	var segArray = recurseCreateSegments(str, segments);
+	console.log('x is', segArray);
+
+	var flag = false;
+
+	segArray.forEach(function(item){
+	if(item.length > 2){
+		if (item === item.split('').reverse('').join('')){
+			flag = true;
+		}
+	}
+	})
+	return flag;
+}
+
+
+
+function recurseCreateSegments(str, segments) {
+	if(str.length === 1) {return segments}
+	for(var index = 0; index < str.length; index++){
+			segments.push(str.substring(index))	
+	}
+	return recurseCreateSegments(str.substring(1), segments)
 }
 
 module.exports = permPalin;
