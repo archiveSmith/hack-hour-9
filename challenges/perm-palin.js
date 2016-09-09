dc828b6816c3b9508a8d8f37d16db8e389863cfd
William Barnes <barnesw91291@gmail.com>
2016-09-09 09:51:19 -0700

    think it works on all edge cases including strings with puncuation and different cases and numbers

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -9,8 +9,30 @@
  *
  */
 
+
 function permPalin(str) {
-	
+  let letters = {};
+  let temp = str.toLowerCase().split('');
+  for (let i = 0; i < temp.length; i++) {
+    if (letters[temp[i]]) {
+      letters[temp[i]]++;
+    } else {
+      letters[temp[i]] = 1;
+    }
+  }
+  const odds = [];
+
+  for (const letter in letters) {
+    if (letters[letter] % 2 !== 0) {
+      odds.push(letter);
+    }
+  }
+  if (odds.length === 0 || odds.length === 1) {
+    return true;
+  }
+  return false;
 }
+
+
 
 module.exports = permPalin;
