afd97658a21cb9f5f2ee4830d4971f25450ad512
dianacheung <diana.cheung.2013@gmail.com>
2016-09-09 09:28:50 -0700

    attempt num-to-words

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -13,7 +13,35 @@
  */
 
 function numToWords(num) {
+  // define lookup table based on numOfDigits
+  var lookup = [{},
+  {'0': 'Zero', '1': 'One', '2': 'Two', '3': 'Three', '4': 'Four', '5': 'Five', '6': 'Six', '7': 'Seven', '8': 'Eight', '9': 'Nine'},
+  {'10': 'Ten', '11': 'Eleven', '12': 'Twelve', '13': 'Thirteen', '14': 'Fourteen', '15': 'Fifteen', '16': 'Sixteen', '17': 'Seventeen', '18': 'Eighteen', '19': 'Nineteen',
+  '20': 'Twenty', '30': 'Thirty',  '40': 'Forty', '50': 'Fifty', '60': 'Sixty', '70': 'Seventy', '80': 'Eighty', '90': 'Ninty'},];
+  
+  var places = ['', '', '', 'Hundred', 'Thousand', 'Thousand', 'Thousand', 'Million', 'Million', 'Million', 'Billion'];
+  
+  // convert num to numStr
+  var numStr = num.toString(10);
+  // convert numStr to char array
+  var numStrArr = numStr.split('');
 
+  
+  return convert(numStrArr);
+  
+  function convert(array) {
+	  // base case
+	  if(array.length === 1) {
+		  
+	  }
+	  
+	  // non base case
+  }
+  
+  
+  
 }
+
+//console.log(numToWords(43));
 
 module.exports = numToWords;
