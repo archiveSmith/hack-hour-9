7bbfdc8b0c79fa2da437caf1c9d990e1b27e1ddb
Jose Neves <joc.neves@gmail.com>
2016-09-09 14:41:05 -0300

    perm palin done

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,29 @@
  */
 
 function permPalin(str) {
-	
-}
+  if (str.length === 1) return true;
+
+  let letters = {};
+  
+  for (let i = 0; i < str.length; i++) {
+  	if (letters[str[i]]) {
+  	  	letters[str[i]] = letters[str[i]] + 1;
+  	} else {
+  	  letters[str[i]] = 1;	
+  	}
+  }
+  
+  let unmatched = 0;
+  for (let key in letters) {
+  	if (letters[key] === 1) {
+  	  unmatched++;
+  	}
+  }
+  
+  if (unmatched > 1) {
+  	return false;
+  }
+  return true;
+} 
 
 module.exports = permPalin;
