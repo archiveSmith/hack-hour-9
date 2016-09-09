c1dddefaace359b53a0dd40f2d79710a69fa2971
Todd Chisom <hello@toddchisom.me>
2016-09-09 10:01:28 -0700

    Palindrome permuation checking

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,30 @@
  */
 
 function permPalin(str) {
-	
+  let centerChar = false;
+  let center;
+  // check to see if there is a even number of each character
+  for (let i = 0; i < str.length; i++){
+    // console.log(str[i]);
+    let char = str[i];
+    let charCount = 0;
+    for (var j = 0; j < str.length; j++) {
+      if (char == str[j]) {
+        charCount++
+      }  
+    }
+    // if there is a odd number and we alread assigned center then return false
+    if (charCount % 2 == 1) {
+      if (centerChar == true && center != char) {
+        return false;
+      } else {
+        centerChar = true;
+        center = char;  
+      }
+    }
+  }
+  // we made it thru so return true
+  return true;
 }
 
 module.exports = permPalin;
