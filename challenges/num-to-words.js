4c02c3a3690f996d57286fa4b36ee67c2027142a
Dana Tran <dana.l.tran@gmail.com>
2016-09-09 08:36:12 -0700

    did not have time to finish num to words

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -13,7 +13,25 @@
  */
 
 function numToWords(num) {
-
+	var result = '';
+	var single = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+	var teens = ['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
+	var digit2 = ['','Ten','Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
+	var digit3 = 'hundred';
+	var digit4 = 'thousand';
+	var digit5 = 'thousand';
+	var digit6 = 'thousand';
+	var digit7 = 'million';
+	if (num < 10) {
+		result += single[num];
+	} else if (num < 20) {
+		result += teens[num-10];
+	} else if (num < 100) {
+		for (var i = 0; i < num.length; i++) {
+			result += digit2[i];
+		}
+	}
+	return result;
 }
 
 module.exports = numToWords;
