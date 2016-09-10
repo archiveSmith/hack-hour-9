2a50d609c56eb9327cbbdce4a25b486a05e9b15b
Jwookie55 <jwookie55@live.com>
2016-09-10 09:46:54 -0700

    commented out test code

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -32,13 +32,13 @@ function rotateGrid(grid, n) {
 }
 
 // tests
-const sampleGrid = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
+// const sampleGrid = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
 
-console.log(sampleGrid);
-rotateGrid(sampleGrid, 3);
+// console.log(sampleGrid);
+// rotateGrid(sampleGrid, 3);
 
 module.exports = rotateGrid;
