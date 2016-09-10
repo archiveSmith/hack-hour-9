b624f462f2890be26eca242c39557186df9709df
Daniel King <daniel.oliver.king@gmail.com>
2016-09-10 09:52:47 -0700

    Works for grids of dimension 3

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -1,14 +1,14 @@
-/* You are given a NxN grid of elements represented by a 2D array. The ith nested array represents 
+/* You are given a NxN grid of elements represented by a 2D array. The ith nested array represents
  * the ith row in the grid.
- * 
- * Write a function to rotate the grid 90 degrees clockwise. 
+ *
+ * Write a function to rotate the grid 90 degrees clockwise.
  *
  * For example:     sampleGrid before:  [   [1, 2, 3],
  *                                          [4, 5, 6],
  *                                          [7, 8, 9]  ]
- *                  
+ *
  *                  rotateGrid(sampleGrid, 3);
- *                  
+ *
  *                  sampleGrid after:   [   [7, 4, 1],
  *                                          [8, 5, 2],
  *                                          [9, 6, 3]  ]
@@ -17,7 +17,33 @@
  */
 
 function rotateGrid(grid, n) {
+  for (let currentRow = 0; currentRow < n / 2; currentRow++) {
+    for (let currentCol = currentRow; currentCol < n / 2; currentCol++) {
 
+      let rowIndex = currentRow;
+      let colIndex = currentCol;
+      let valueToInsert = grid[rowIndex][colIndex];
+      let valueToKeep;
+      for (let i = 0; i < 4; i++) {
+        const {newRowIndex, newColIndex} = getNewPosition(rowIndex, colIndex, n);
+        valueToKeep = grid[newRowIndex][newColIndex];
+        grid[newRowIndex][newColIndex] = valueToInsert;
+
+        rowIndex = newRowIndex;
+        colIndex = newColIndex;
+        valueToInsert = valueToKeep;
+      }
+    }
+  }
+
+  return grid;
+}
+
+function getNewPosition(rowIndex, colIndex, n) {
+  const newRowIndex = colIndex;
+  const newColIndex = n - 1 - rowIndex;
+
+  return {newRowIndex, newColIndex}
 }
 
 module.exports = rotateGrid;
