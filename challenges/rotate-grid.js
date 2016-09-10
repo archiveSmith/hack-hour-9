8713d5498498e9800f6f7630073156a55e2fe58c
Daniel King <daniel.oliver.king@gmail.com>
2016-09-10 10:01:31 -0700

    Solution works for different size arrays

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -16,28 +16,28 @@
  * BONUS: Do this in place
  */
 
-function rotateGrid(grid, n) {
-  for (let currentRow = 0; currentRow < n / 2; currentRow++) {
-    for (let currentCol = currentRow; currentCol < n / 2; currentCol++) {
+ function rotateGrid(grid, n) {
+   for (let currentRow = 0; currentRow < n / 2; currentRow++) {
+     for (let currentCol = currentRow; currentCol < n - 1 - currentRow; currentCol++) {
 
-      let rowIndex = currentRow;
-      let colIndex = currentCol;
-      let valueToInsert = grid[rowIndex][colIndex];
-      let valueToKeep;
-      for (let i = 0; i < 4; i++) {
-        const {newRowIndex, newColIndex} = getNewPosition(rowIndex, colIndex, n);
-        valueToKeep = grid[newRowIndex][newColIndex];
-        grid[newRowIndex][newColIndex] = valueToInsert;
+       let rowIndex = currentRow;
+       let colIndex = currentCol;
+       let valueToInsert = grid[rowIndex][colIndex];
+       let valueToKeep;
+       for (let i = 0; i < 4; i++) {
+         const {newRowIndex, newColIndex} = getNewPosition(rowIndex, colIndex, n);
+         valueToKeep = grid[newRowIndex][newColIndex];
+         grid[newRowIndex][newColIndex] = valueToInsert;
 
-        rowIndex = newRowIndex;
-        colIndex = newColIndex;
-        valueToInsert = valueToKeep;
-      }
-    }
-  }
+         rowIndex = newRowIndex;
+         colIndex = newColIndex;
+         valueToInsert = valueToKeep;
+       }
+     }
+   }
 
-  return grid;
-}
+   return grid;
+ }
 
 function getNewPosition(rowIndex, colIndex, n) {
   const newRowIndex = colIndex;
