9ccf545467be6c159a3ab254c5c168e16ddab263
William Barnes <barnesw91291@gmail.com>
2016-09-10 10:16:40 -0700

    not in place. will work on in place later

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -15,9 +15,28 @@
  *
  * BONUS: Do this in place
  */
-
+// 'use strict';
 function rotateGrid(grid, n) {
+  //no in place
+  const newGrid = [];
+  for (let i = 0; i < n; i++) {
+    newGrid.push([]);
+  }
+  for (let i = 0; i < n; i++) {
+    for (let j = n - 1; j >= 0; j--) {
+      newGrid[i].push(grid[j][i]);
+    }
+  }
 
+  return newGrid;
 }
+
+// let array1 = [[1]];
+// let array2 = [[1, 2], [3, 4]];
+// let array3 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+// let array4 = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]];
+// let array5 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]];
+
+// console.log(rotateGrid(array3, 3));
 
 module.exports = rotateGrid;
