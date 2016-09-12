eecbf7778d39923a8bbbc6dbd48f89cfb639536f
Stephen Langdon <stphnlngdncoding@gmail.com>
2016-09-12 09:48:39 -0700

    finished stack-queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,15 @@
 
 
 function Stack() {
-
+	this.data = [];
+	this.length = 0;
+	this.pop = function() {
+		let result = this.data.pop()
+		return result;
+	}
+	this.push = function(val) {
+		this.data.push(val);
+	}
 }
 
 
@@ -14,7 +22,24 @@ function Stack() {
 
 
 function Queue() {
-
+	this.s1 = new Stack();
+	this.s2 = new Stack();
+	this.enqueue = function(val) {
+		this.s1.push(val);
+	}
+	
+	this.dequeue = function() {
+		let result;
+		if (this.s2.length > 0) {
+			result = this.s2.pop();
+		} else {
+			while (this.s1.data.length > 0) {
+				this.s2.push(this.s1.pop());
+			}
+			result = this.s2.pop();
+		}
+		return result;
+	}
 }
 
 module.exports = {Stack: Stack, Queue: Queue};
