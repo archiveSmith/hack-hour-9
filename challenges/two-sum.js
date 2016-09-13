3c115e1a99060e5637d7dab06857ce58ab1805f1
sneakykate <sneakyk8@gmail.com>
2016-09-13 09:51:53 -0700

    twoSum with for loops

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,22 @@
  */
 
 function twoSum(arr, n) {
-
+  if (arr.length != 0){
+    if(typeof n === 'number'){
+      for ( let i = 0; i < arr.length-1; i ++){
+        for ( let j = 1; j < arr.length; j++){
+          if (arr[i] + arr[j] === n){
+            return true;
+          }
+        }
+      }
+    return false; // if it's a number but nothing adds up to it
+    }
+    else {
+    	return undefined; // if n isn't a number
+    }
+   return false; // if there's nothing in the array
+  }
 }
 
 module.exports = twoSum;
