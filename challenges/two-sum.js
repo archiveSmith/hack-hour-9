4a7172296867a3774b2b76d70e583feb078e8063
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-09-13 09:38:55 -0700

    completed two sum challenge

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,21 @@
  */
 
 function twoSum(arr, n) {
-
+	arr.sort(function(a, b){return a-b});
+	var i = 0
+	var j = arr.length-1
+	while(i < j){
+   	if      (arr[i] + arr[j] == n){
+   		return true;	
+   	} 
+   	else if (arr[i] + arr[j] <  n){ 
+   		i++;
+  	}
+  	else if (arr[i] + arr[j] >  n){ 
+   		j--;
+   	}
+	}
+	return false
 }
 
 module.exports = twoSum;
