bd6773038b714467680f3763bd9be0b1fd5a6d70
jaycetan <jaycetan@gmail.com>
2016-09-13 09:34:41 -0700

    first pass aat two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,26 @@
  */
 
 function twoSum(arr, n) {
+  if (Number.isInteger(n)) {
+    return undefined;
+  };
+  arr = arr.filter(isBigEnough);
 
-}
+  function isBigEnough(value) {
+    return n >= value;
+  }
+
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < array.length; j++) {
+      if (arr[i] + arr[j] === num) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
+console.log(twoSum([2, 3, 4, 6, 25, 87, 4], 15));
 
 module.exports = twoSum;
