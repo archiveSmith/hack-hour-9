45fcaf2dfb43438a9a930d9ca27d38378fd52a9e
Simon Situ <ssitu001@gmail.com>
2016-09-13 16:04:56 -0700

    complete twoSum in brute force, need to refactor

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -4,6 +4,23 @@
 
 function twoSum(arr, n) {
 
+  let differences = {};
+  
+  for (let i = 0; i < arr.length; i++) {
+    let difference = n - arr[i];
+    differences[arr[i]] = difference;
+  }
+  
+  for (let key in differences) {
+  	if (arr.indexOf(differences[key]) > -1) {
+  		return true;
+  	}
+  }
+  return false;
+
 }
+
+let nums = [4,1,1,1,2];
+twoSum(nums, 6)
 
 module.exports = twoSum;
