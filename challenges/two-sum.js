492567fd88d657899ef0cb9881030ff468a62290
Jwookie55 <jwookie55@live.com>
2016-09-13 09:35:44 -0700

    finished two sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -2,8 +2,39 @@
  *  array that sum up to the target value; return false otherwise
  */
 
-function twoSum(arr, n) {
+// brute force, lower space complexity
+// function twoSum(arr, n) {
+//   if (typeof n !== 'number') {
+//     return undefined;
+//   }
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === n) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
 
+// better time complexity, higher space complexity
+function twoSum(arr, n) {
+  const arrHash = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (!arrHash) {
+      arrHash[arr[i]] = i;
+    }
+  }
+  for (let j = 0; j < arr.length; j++) {
+    if (arrHash[n - arr[j]]) {
+      return true;
+    }
+  }
+  return false;
 }
+
+// test data
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// twoSum(nums, 10)
 
 module.exports = twoSum;
