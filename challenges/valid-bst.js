464a3b3297aabf6fc538d3c4957dfed1bce42a98
Daniel King <daniel.oliver.king@gmail.com>
2016-09-14 09:58:43 -0700

    Finished valid-bst and two-sum

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -3,7 +3,7 @@
  *      at any given node, the value of all the nodes in its left tree must be <= its value
  *      at any given node, the value of all the nodes in its right tree must be > its value
  */
- 
+
 
 function BinaryTree(val) {
     this.value = val;
@@ -12,7 +12,17 @@ function BinaryTree(val) {
 }
 
 function validBST(tree) {
+  // Check if the left tree exists, and if so if it is invalid
+  if (tree.left) {
+    if (tree.left.value > tree.value || !validBST(tree.left)) return false;
+  }
 
+  // Check if the right tree exists, and if so if it is invalid
+  if (tree.right) {
+    if (tree.right.value <= tree.value || !validBST(tree.right)) return false;
+  }
+
+  return true;
 }
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
