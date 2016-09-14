0bc33bd562c596785990c61a3217b5251d040a58
dianacheung <diana.cheung.2013@gmail.com>
2016-09-14 10:00:05 -0700

    finished valid-bst

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -12,7 +12,59 @@ function BinaryTree(val) {
 }
 
 function validBST(tree) {
+    var isLeftValid = true;
+    var isRightValid = true;
 
+    // edge case
+    if(tree === null) {
+        return false;
+    }
+
+    // check if left side valid
+    if(tree.left !== null) { // check if left child exists
+        if(tree.left.value <= tree.value) { // valid
+            // recursive check
+            isLeftValid = validBST(tree.left);
+        } else { // invalid left
+            isLeftValid = false;
+        }
+    }
+
+    // check if right side valid
+    if(tree.right !== null) { // check if right child exists
+        if(tree.right.value > tree.value) { // valid
+            // recursive check
+            isRightValid = validBST(tree.right);
+        } else { // invalid right
+            isRightValid = false;
+        }
+    }
+    
+    // if either side is invalid, then false is returned
+    return isLeftValid && isRightValid;
 }
+
+/* tests */
+/*
+var root = new BinaryTree(10);
+root.left = new BinaryTree(5);
+root.right = new BinaryTree(15);
+
+console.log(validBST(root)); // expect true
+
+root.left.left = new BinaryTree(4);
+root.left.right = new BinaryTree(7);
+root.right.left = new BinaryTree(12);
+
+console.log(validBST(root)); // expect true
+
+root.right.right = new BinaryTree(14);
+
+console.log(validBST(root)); // expect false
+
+root.left.left.right = new BinaryTree(2);
+
+console.log(validBST(root)); // expect false
+*/
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
