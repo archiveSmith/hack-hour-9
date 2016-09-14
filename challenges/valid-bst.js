86eb43d67e2c1dce9761328358de239086bda42d
shawn-southwell <shawn.southwel@gmail.com>
2016-09-14 10:26:21 -0700

    finished valid bst

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -11,7 +11,28 @@ function BinaryTree(val) {
     this.right = null;
 }
 
+BinarySearchTree.prototype.add = function (value) {
+  let right = this.value < value;
+  if (right && this.right === null) {
+    this.right = new BinarySearchTree(value);
+  } else if (!right && this.left === null) {
+    this.left = new BinarySearchTree(value);
+  } else {
+    if (right) {
+      this.right.add(value);
+    } else {
+      this.left.add(value);
+    }
+  }
+
+};
+
 function validBST(tree) {
+if (tree.value === null) return true;
+if (tree.left > tree.value) return false;
+if (tree.right <= tree.value) return false;
+return validBST(tree.left) && validBST(tree.right);
+
 
 }
 
