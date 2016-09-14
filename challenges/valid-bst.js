a61fccebbd51b67b5f68f0903d345e0c47aa0b97
Simon Situ <ssitu001@gmail.com>
2016-09-14 10:32:58 -0700

    validbst wip

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -12,7 +12,40 @@ function BinaryTree(val) {
 }
 
 function validBST(tree) {
+	let root = tree.value;
+	
+
+	function inner(innertree, root) {
+		root =  root || tree.value; 
+	if(tree.left) {
+		if(innertree.left.value <= root) {
+			inner(innertree.left, innertree.left.value);
+		} else {
+			return false;
+		}
+	} 
+	
+	if(tree.right) {
+		if(innertree.right.value > root) {
+			inner(innertree.right, innertree.right.value);
+		} else {
+			return false;
+		}
+	}
+	return true;
+	}
+	inner(tree, root);
+	
 
 }
+
+var bst = new BinaryTree(8);
+bst.left = new BinaryTree(4);
+bst.left.left = new BinaryTree(2);
+bst.right = new BinaryTree(9);
+bst.right.right = new BinaryTree(12);
+//console.log(bst);
+
+validBST(bst)
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
