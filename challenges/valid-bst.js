6ecaea0f7947dfc23e4c478cfd37b2efe05bd978
Jwookie55 <jwookie55@live.com>
2016-09-14 10:21:43 -0700

    finished validBST needs work

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -3,16 +3,25 @@
  *      at any given node, the value of all the nodes in its left tree must be <= its value
  *      at any given node, the value of all the nodes in its right tree must be > its value
  */
- 
 
 function BinaryTree(val) {
-    this.value = val;
-    this.left = null;
-    this.right = null;
+  this.value = val;
+  this.left = null;
+  this.right = null;
 }
 
 function validBST(tree) {
-
+  if (!tree || (!tree.left && !tree.right)) return true;
+  if ((!tree.left || tree.left.value <= tree.value) && (!tree.right || tree.right.value > tree.value)) {
+    return (validBST(tree.right) && validBST(tree.left));
+  }
+  return false;
 }
+
+// tests
+// let newTree = new BinaryTree(6);
+// newTree.left = new BinaryTree(5);
+// newTree.right = new BinaryTree(7);
+// validBST(newTree);
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
