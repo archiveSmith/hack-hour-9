6d80595e078aeea8eb49569ed334a3e1d1d7eef6
Simon Situ <ssitu001@gmail.com>
2016-09-13 18:29:56 -0700

    refactor but still o(n^2)

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -4,23 +4,21 @@
 
 function twoSum(arr, n) {
 
-  let differences = {};
-  
-  for (let i = 0; i < arr.length; i++) {
-    let difference = n - arr[i];
-    differences[arr[i]] = difference;
-  }
-  
-  for (let key in differences) {
-  	if (arr.indexOf(differences[key]) > -1) {
-  		return true;
-  	}
-  }
-  return false;
-
+	let difference = n - arr[0];
+	let sliced = arr.slice(1);
+	
+	while(sliced.length) {
+		if (sliced.indexOf(difference) > -1) {
+			return true;
+		} else {
+			difference = n-sliced[0]
+			sliced = sliced.slice(1);
+		}
+	}
+	return false;
 }
 
-let nums = [4,1,1,1,2];
-twoSum(nums, 6)
+let nums = [1,2,3,4,4];
+twoSum(nums, 7)
 
 module.exports = twoSum;
