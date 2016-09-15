b9a1f3f9c7f480daf9e43aeeb47ad6535867ef19
sneakykate <sneakyk8@gmail.com>
2016-09-15 09:32:23 -0700

    using for loop & math.abs

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,25 @@
  */
 
 function gcd(a, b) {
+  var higher;
+  var lower;
+  //determine which one's higher
+  if (Math.abs(a)>Math.abs(b)){
+    higher = a;
+    lower = b;
+  }
+  else{ 
+    higher = b;
+    lower = a;
+  }
 
+  for(let i = Math.abs(lower); i >= 1; i--){
+    if (higher % i === 0 && lower % i === 0){
+      return i;
+    }
+  }
+  return 'none found';
 }
+
 
 module.exports = gcd;
