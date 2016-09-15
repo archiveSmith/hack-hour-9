54f55d5b17f5b71a77aa993de40ea65b3cf5c1e6
Derrick Ung <derrickrung@yahoo.com>
2016-09-15 10:05:26 -0700

    gcd refactored

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,29 +8,12 @@
  */
 
 function gcd(a, b) {
-	if(a % b === 0) {
-		return b
-	}
 
-	if(b % a === 0) {
-		return a;
-	}
+	let num = Math.max(a, b);
+	let div = Math.min(a, b);
+	let remain = num % div;
 
-	let remain = a % b;
-	let newRemain;
-	let div = b;
-	let ans;
-	
-	while(newRemain !== 0) {
-		newRemain = div % remain;
-		if(newRemain === 0) {
-			ans = remain;
-		}
-		div = remain;
-		remain = newRemain;
-	}
-
-	return ans;
+	return remain === 0 ? div : gcd(div, remain)
 }
 
 module.exports = gcd;
