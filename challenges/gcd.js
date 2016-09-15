95ab88d6a96ec6f54c885fdf058366601d06a726
William Barnes <barnesw91291@gmail.com>
2016-09-15 09:56:55 -0700

    adding a non recursive solution that works in the same manner

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,9 +8,18 @@
  */
 
 function gcd(a, b) {
-  if (!b) return a;
-  return gcd(b, a % b);
+  // non recursive working in the same manner
+  while (b !== 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+  // if (!b) return a;
+  // return gcd(b, a % b);
 }
+
+console.log(gcd(10, 100000000000000));
 
 
 module.exports = gcd;
