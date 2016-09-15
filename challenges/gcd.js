feeff45818de6f1c01fbc02f4c2183a04d2cd74f
Dana Tran <dana.l.tran@gmail.com>
2016-09-15 09:33:33 -0700

    completed gcd hh

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,26 @@
  */
 
 function gcd(a, b) {
-
+  var small, big;
+  if (a === 0) {
+    return b;
+  }
+  if (b === 0){
+    return a;
+  }
+  if (Math.abs(a) > Math.abs(b)) {
+    small = Math.abs(b);
+    big = Math.abs(a);
+  } else {
+    small = Math.abs(a);
+    big = Math.abs(b);
+  }
+  console.log('big: ', big, 'small: ', small);
+  for (var i = small; i > 0; i--){
+    if (big % i === 0 && small % i === 0){
+      return i;
+    }
+  }
 }
 
 module.exports = gcd;
