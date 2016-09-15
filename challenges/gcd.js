7dbd33f72f1413909392354717fd529f67c73dd2
Jwookie55 <jwookie55@live.com>
2016-09-15 09:58:35 -0700

    refactored gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -12,23 +12,19 @@ function gcd(a, b) {
   } else if (b % a === 0) {
     return Math.abs(a);
   }
-  let output = -Infinity;
   const newA = Math.abs(a);
   const newB = Math.abs(b);
-  const done = Math.min(newA, newB);
-  for (let i = 0; i <= done; i++) {
-    if (newA % i === 0 && newB % i === 0) {
-      output = i;
-    }
+  const diff = Math.max(newA, newB) - Math.min(newA, newB);
+  for (let i = diff; i > 0; i--) {
+    if (newA % i === 0 && newB % i === 0) return i;
   }
-  return output;
 }
 
 // test data
-// console.log(gcd(10, 8), '= 2');
-// console.log(gcd(10, 9), '= 1');
-// console.log(gcd(10, -5), '= 5');
-// console.log(gcd(-10, 20), '= 10');
-// console.log(gcd(9, 5), '= 1');
+console.log(gcd(10, 8), '= 2');
+console.log(gcd(10, 9), '= 1');
+console.log(gcd(10, -5), '= 5');
+console.log(gcd(-10, -20), '= 10');
+console.log(gcd(9, 5), '= 1');
 
 module.exports = gcd;
