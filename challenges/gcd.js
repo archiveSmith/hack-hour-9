0bd1d5263e8d816d5770b58d109ba2c56ca0d380
medic27 <jczhang@ucla.edu>
2016-09-15 10:58:55 -0700

    resubmit

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -11,7 +11,6 @@ function gcd(a, b) {
 	let arr = [];
 
 	function getPrimeFactors(c, d, n) {
-		console.log(c,d,n);
 		if (n > c || n > d) {
 		} else if (c === 1 || d === 1) {
 			arr.push(1);
@@ -29,17 +28,5 @@ function gcd(a, b) {
 	return arr.reduce(function(prev,next){return prev*next;});
 }
 
-gcd(8,8);
-
-//get all prime factors, then the intersection has the GCD
-//start with prime factors for one of them
-//loop thru from 2 to a, 
-
-function primeFactors(a) {
-	let n = a.length;
-	for (let i = 0; i < n/; i++) {
-
-	}
-}
 
 module.exports = gcd;
