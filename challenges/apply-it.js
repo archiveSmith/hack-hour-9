6558727f246ce4ec47e2c761c33c19b03dd48f4d
Todd Chisom <hello@toddchisom.me>
2016-09-16 10:06:20 -0700

    Hacky solution

--- challenges/apply-it.js
+++ challenges/apply-it.js
@@ -27,6 +27,12 @@
 
 function applyIt(func, args) {
 
+return function(){
+	let argsString = args.toString();
+	
+	return func(args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7],args[8],args[9],args[10])
+}
+
 }
 
 module.exports = applyIt;
