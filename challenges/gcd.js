2ddb49d1132366986e1739cc0c414db4852766f9
Dlaosb <Dlaosb@gmail.com>
2016-09-22 01:36:31 -0700

    Refactored GCD, checking answer for ApplyIt

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,14 +8,19 @@
  */
 
 function gcd(a, b) {
-	let min = Math.min(Math.abs(a,b)); 
-	while(min >= 0) {
-		if(a % min === 0 && b % min === 0){
-			return min; 
-		}
-		min--;
-	}
-	return min;
+	// let min = Math.min(Math.abs(a,b)); 
+	// while(min >= 0) {
+	// 	if(a % min === 0 && b % min === 0){
+	// 		return min; 
+	// 	}
+	// 	min--;
+	// }
+	// return min;
+	// 	//absolute
+	if(a === 0) return Math.abs(b); 
+	if(b === 0) return Math.abs(a); 
+	
+	return gcd(b, a % b);
 }
 
 module.exports = gcd;
