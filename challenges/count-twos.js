84aa87cd52c9d3eebd99160b25997e5efa1a7cb1
dianacheung <diana.cheung.2013@gmail.com>
2016-09-26 09:40:51 -0700

    finished count-twos, brute force solution

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -8,7 +8,30 @@
 
 
 function countTwos(num) {
-
+ // brute force solution: loop from 1 to n, if digit contains a '2', then keep track of it
+ var count = 0;
+ 
+ for(var i = 1; i <= num; ++i) {
+	var nStr = i.toString();
+	 
+	var found = nStr.match(/[2]/gi); // match '2'
+	
+	if(found !== null) { // there were match(es)
+		count += found.length;
+	}	 
+ }
+ 
+ return count;
+ 
 }
+
+/* tests */
+/*
+console.log(countTwos(1));  //-> 0
+console.log(countTwos(3));  //-> 1
+console.log(countTwos(13));  //-> 2
+console.log(countTwos(1000)); // -> 300
+console.log(countTwos(11420)); // -> 4483
+*/
 
 module.exports = countTwos;
