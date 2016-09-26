74a7c10aa6caeaeb6f4580089c3a76c5868d8c73
William Barnes <barnesw91291@gmail.com>
2016-09-26 09:22:58 -0700

    should work

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -2,11 +2,11 @@
 // The function will return the number of 2s encountered when counting
 // from 1 to n (inclusive). 2222 counts as having 4 2s.
 
-console.log(countTwos(1)); //  -> 0
-console.log(countTwos(3)); //  -> 1
-console.log(countTwos(13)); // -> 2
-console.log(countTwos(1000)); // -> 300
-console.log(countTwos(11420));  // -> 4483
+// console.log(countTwos(1)); //  -> 0
+// console.log(countTwos(3)); //  -> 1
+// console.log(countTwos(13)); // -> 2
+// console.log(countTwos(1000)); // -> 300
+// console.log(countTwos(11420));  // -> 4483
 
 
 function countTwos(num) {
