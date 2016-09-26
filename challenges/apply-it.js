7a3ae246874eb93d5a8396df584d31e37ff88b19
Derrick Ung <derrickrung@yahoo.com>
2016-09-26 15:58:55 -0700

    apply-it complete

--- challenges/apply-it.js
+++ challenges/apply-it.js
@@ -26,7 +26,7 @@
  */
 
 function applyIt(func, args) {
-
+	return func(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11])
 }
 
 module.exports = applyIt;
