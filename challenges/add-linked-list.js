d6a98e646025d2f2dcc07884d804d0cd44e06236
yantonsoup <ygoldobin@gmail.com>
2016-09-27 09:50:21 -0700

    add-linked-list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -14,7 +14,36 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
-
+	var carry = 0;
+	var valToSet = 0;
+	
+	while(l1.next && l2.next) {
+		if(!head) {
+			if (l1.value + l2.value >= 10) {
+				carry = 1;
+				valToSet = (l1.value + l2.value) % 10;
+			}
+			else {valToSet = l1.value + l2.value}
+			var head = new Node(valToSet);
+			var current = head;
+		} else {
+			var prevCarry = carry;
+			l1 = l1.next;
+			l2 = l2.next;
+			
+			if (l1.value + l2.value >= 10) {
+				carry = 1;
+				valToSet = (l1.value + l2.value) % 10 + prevCarry;
+			}
+			else {valToSet = l1.value + l2.value + prevCarry}
+			
+			let temp = new Node(valToSet)
+			current.next = temp;
+			current = current.next;
+			
+			}
+		}
+	return head;
 }
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
