fb19cc486ca0499c88e1760f64143cc556e29a2e
William Barnes <barnesw91291@gmail.com>
2016-09-27 09:33:25 -0700

    should work, but not the prettiest code

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -14,7 +14,45 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
-
+  let l1Length = 0;
+  let l2Length = 0;
+  for (let i = l1; i; i = i.next) {
+    l1Length++;
+  }
+  for (let i = l2; i; i = i.next) {
+    l2Length++;
+  }
+  if (l1Length > l2Length) {
+    const dif = l1Length - l2Length;
+    for (let i = 0; i < dif; i++) {
+      const node = new Node(0);
+      node.next = l2;
+      l2 = node;
+    }
+  }
+  if (l2Length > l1Length) {
+    const dif = l2Length - l1Length;
+    for (let i = 0; i < dif; i++) {
+      const node = new Node(0);
+      node.next = l1;
+      l1 = node;
+    }
+  }
+  for (let i = l1, j = l2; i; i = i.next, j = j.next) {
+    i.value += j.value;
+    if (i.value >= 10) {
+      i.value -= 10;
+      if (i.next) {
+        i.next.value += 1;
+      } else {
+        const node = new Node(1);
+        i.next = node;
+        break;
+      }
+    }
+  }
+  return l1;
 }
 
-module.exports = {Node: Node, addLinkedList: addLinkedList};
+
+module.exports = { Node, addLinkedList };
--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -2,18 +2,19 @@
 // The function will return the number of 2s encountered when counting
 // from 1 to n (inclusive). 2222 counts as having 4 2s.
 
-// console.log(countTwos(1)); //  -> 0
-// console.log(countTwos(3)); //  -> 1
-// console.log(countTwos(13)); // -> 2
-// console.log(countTwos(1000)); // -> 300
-// console.log(countTwos(11420));  // -> 4483
+console.log(countTwos(1)); //  -> 0
+console.log(countTwos(3)); //  -> 1
+console.log(countTwos(13)); // -> 2
+console.log(countTwos(1000)); // -> 300
+//console.log(countTwos(107653400376320956));  // -> 4483
 
 
 function countTwos(num) {
   let twos = 0;
   for (let i = 0; i <= num; i++) {
-    const numStr = i.toString();
-    for (let j = 0; j < numStr.length; j++) if (numStr[j] === '2') twos++;
+    i.toString().replace(/2/g, () => {
+      twos++;
+    });
   }
   return twos;
 }
