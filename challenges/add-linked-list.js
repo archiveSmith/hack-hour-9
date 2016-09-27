26b2dc00323a04976042a86e53af29f39e8dc356
sneakykate <sneakyk8@gmail.com>
2016-09-27 09:44:39 -0700

    add linked list take apart put back together

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -14,7 +14,30 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
-
+  let l1Arr = [l1.value];
+  let l2Arr = [l2.value];
+  while (l1.next != null) {
+// get the values into an array instead.
+    l1Arr.push(l1.next.value);
+    l1.next = l1.next.next;
+  }
+  while (l2.next != null) {
+    // get the values into an array instead.
+    l2Arr.push(l2.next.value);
+    l2.next = l2.next.next;
+  }
+  const l1Num = Number(l1Arr.reverse().join(''));
+  const l2Num = Number(l2Arr.reverse().join(''));
+  const l1l2Sum = l1Num + l2Num;
+  const l1l2SumStr = l1l2Sum.toString();
+  const len = l1l2SumStr.length;
+  const output = new Node();
+  for (let i = 0, ss = l1l2SumStr, pointer = output; i < len; i++) {
+    pointer.value = Number(ss.charAt(len - 1 - i));
+    pointer.next = new Node();
+    pointer = pointer.next;
+  }
+  return output;
 }
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
