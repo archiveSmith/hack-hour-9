57d1b5caa8e673616f3a83d34067de25e10100e7
Todd Chisom <hello@toddchisom.me>
2016-09-27 20:47:50 -0700

    adding linking list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -14,6 +14,32 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  function addList(l1, l2, carry) {
+    if (!l1 && !l2) return null;
+    if (!l1) return l2;
+    if (!l2) return l1;
+    let sum = l1.value + l2.value;
+    let head = new Node(sum);
+    head.next = addList(l1.next, l2.next)
+    return head;
+  }
+  function reduceList(l) {
+    let carry = 0;
+    let tail = l;
+    for (let node = l; node; node = node.next) {
+      node.value = node.value + carry;
+      if (node.value > 9) {
+        carry = Math.floor(node.value / 10);
+        node.value = node.value % 10;
+      }
+      tail = node;
+    }
+    if (carry) {
+      tail.next = new Node(carry);
+    }
+    return l;
+  }
+  return reduceList(addList(l1, l2))
 
 }
 
