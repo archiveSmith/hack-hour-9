ad63f585b29c10c2fd2a4b11e7ee8d2a40d5aca5
Weylin Wagnon <weylinwagnon@gmail.com>
2016-09-29 09:27:54 -0700

    finished in O(n) time

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -24,3 +24,4 @@ function checkDepth(tree, depth = 0) {
 
 
 module.exports = { BinaryTree: BinaryTree, superbalanced: superbalanced };
+magnet:?xt=urn:btih:62235d1630b22fdb5d651257bca47ccc8d3e5d52&dn=Mr.Robot.S02E12.PROPER.HDTV.x264-KILLERS%5Bettv%5D&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969
