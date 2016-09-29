628481e9a1ad33584b483139327cfc1f4ae783bf
Weylin Wagnon <weylinwagnon@gmail.com>
2016-09-29 09:54:10 -0700

    finished in O(n) time

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -10,8 +10,20 @@
  *
  */
 
+// function repeatNumbers(array) {
+//   const store = {};
+//   for (let i = 0; i < array.length; i++) {
+//     if (store[array[i]]) return array[i];
+//     store[array[i]] = true;
+//   }
+// }
 function repeatNumbers(array) {
-
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+  	sum += array[i];
+  	sum -= i + 1;
+  	if (sum !== 0) return array[i];
+  }
 }
 
 module.exports = repeatNumbers;
