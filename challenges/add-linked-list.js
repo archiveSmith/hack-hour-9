2c12d788b82ca5e7bc3026a5ba006098996fb342
Dlaosb <Dlaosb@gmail.com>
2016-09-29 09:22:50 -0700

    Fixing commit

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -14,6 +14,7 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+<<<<<<< HEAD
 	if(l1 === null && l2 === null) return undefined;
 	sumL1 = parseInt(reverseLinkedList(l1));
 	sumL2 = parseInt(reverseLinkedList(l2));
@@ -39,6 +40,9 @@ function reverseLinkedList(head) {
 		currentNode = currentNode.next; 
 	}
 	return newAr.reverse().join(''); 
+=======
+
+>>>>>>> ed73535131c71dd086899633c223a3284482d23e
 }
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
