680591df86f31e36afe820d3d393b732fa152da6
yantonsoup <ygoldobin@gmail.com>
2016-09-28 18:52:56 -0700

    :(

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -14,7 +14,31 @@ function BinaryTree(value) {
 }
 
 function superbalanced(tree) {
-
+	if (tree === null) return false;
+	var leftHeight = goDown(tree.left);
+	var rightHeight = goDown(tree.right);
+  if (Math.abs(leftHeight-rightHeight) > 1){
+			return false;
+	}
+	return true;
 }
+
+
+  function goDown(treeLeft, treeRight, height){ 
+  	if (treeNode === null) return 0;
+  	else {
+  		return Math.max(goDown(treeNode.left), goDown(treeNode.right)) + 1;
+  	}
+
+  }
+
+// var myBST = new BinaryTree(1)
+// myBST.left = new BinaryTree(4)
+// myBST.right = new BinaryTree(7)
+// myBST.left.left = new BinaryTree(11)
+// myBST.left.right = new BinaryTree(1)
+// myBST.right.left = new BinaryTree(6)
+// myBST.right.right = new BinaryTree(1)
+
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
