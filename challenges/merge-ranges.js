87a46edd52032fedd4c9cb3dfafaf19f96158cfe
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-09-30 10:02:26 -0700

    finished merge arrays

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -10,8 +10,22 @@
  */
 
 
-function mergeRanges(array) {
+function mergeRanges(ranges) {
+  var result = [];
+  ranges.sort(function (a, b) {
+    return a[0] - b[0];
+  })
+
+  ranges.forEach(function (r) {
+    if (!result.length || r[0] > result[result.length - 1][1])
+      result.push(r);
+    else
+      result[result.length - 1][1] = r[1];
+  });
+
+  return result;
 
 }
+
 
 module.exports = mergeRanges;
