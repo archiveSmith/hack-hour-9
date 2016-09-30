b798761ad4d1cf4e310148e7ee6c94075c140102
Daniel King <daniel.oliver.king@gmail.com>
2016-09-30 10:17:22 -0700

    Finished merge-ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -9,9 +9,26 @@
  * Do not assume the ranges are in order
  */
 
+ function mergeRanges(array) {
+   array.sort((a, b) => a[0] - b[0]);
 
-function mergeRanges(array) {
+   let startTimes = [array[0][0]];
+   let endTimes = [Math.max(...array.map(range => range[1]))];
 
-}
+   for (let i = 1; i < array.length; i++) {
+   	if (array[i][0] > array[i - 1][1]) startTimes.push(array[i][0]);
+   }
+   for (let i = array.length - 2; i >= 0; i--) {
+   	if (array[i][1] < array[i + 1][0]) endTimes.push(array[i][1]);
+   }
+
+   return zip(startTimes, endTimes.reverse());
+ }
+
+ function zip(arr1, arr2) {
+ 	return arr1.map((_, i) => {
+ 		return [arr1[i], arr2[i]];
+ 	});
+ }
 
 module.exports = mergeRanges;
