4c3f5731afabc052a7bf023c7657836baf2057c1
William Barnes <barnesw91291@gmail.com>
2016-09-30 09:33:50 -0700

    should be good to go, NINJAZ

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,17 @@
 
 
 function mergeRanges(array) {
-
+  array.sort((a, b) => a[0] - b[0]);
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i][1] >= array[i + 1][0]) {
+      const first = array[i][0];
+      const second = array[i + 1][1];
+      const inner = [first, second];
+      array.splice(i, 2, inner);
+    }
+  }
+  return array;
 }
+
 
 module.exports = mergeRanges;
