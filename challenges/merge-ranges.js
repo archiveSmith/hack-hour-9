aff280c8e87fc7b7a374cf54ca96adaedbbeb841
shawn-southwell <shawn.southwel@gmail.com>
2016-09-30 10:37:47 -0700

    merge range

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -10,8 +10,20 @@
  */
 
 
-function mergeRanges(array) {
-
+function mergeRanges(arr) {
+	let sorted = arr.sort((a,b) => a[0] - b[0])
+	let newArr = [];
+	sorted.forEach((time,idx,array) => {
+		let range = [...Array(time[1]+1).keys()].slice(time[0]);
+		if (array[idx+1] && (range.includes(array[idx+1][0]) || range.includes([array[idx+1][1]]))){
+			newArr.push([array[idx][0],array[idx+1][1]]);
+			delete array[idx+1];
+		 } else {
+		 	newArr.push(time);
+		 }
+	})
+	return newArr;
 }
+
 
 module.exports = mergeRanges;
