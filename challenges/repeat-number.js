b7ddb2ce8940ec013ccb3ee209df9bc773be6e44
dianacheung <diana.cheung.2013@gmail.com>
2016-09-30 09:21:43 -0700

    finished repeat-number

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,32 @@
  */
 
 function repeatNumbers(array) {
+ // sort this array
+ array.sort(function(a, b) {
+	if(a < b) {
+		return -1;
+	} else if(a > b) {
+		return 1;
+	} else {
+		return 0;
+	}
+ });
 
+ // iterate through sorted array and check if current array element equals to what's stored in prevSeen
+ var prevSeen;
+
+ for(var i = 0; i < array.length; ++i) {
+	if(array[i] === prevSeen) {
+		return array[i];
+	}
+	
+	prevSeen = array[i];
+ }
+ 
 }
+/*
+var a1 = [4,2,5,6,7,4];
+console.log(repeatNumbers(a1));
+*/
 
 module.exports = repeatNumbers;
