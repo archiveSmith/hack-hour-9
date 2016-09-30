5c583c28fdfaf301826188d6874c321fef2b09a2
Jwookie55 <jwookie55@live.com>
2016-09-30 09:29:46 -0700

    finished merge ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,33 @@
 
 
 function mergeRanges(array) {
-
+  const start = [];
+  const end = [];
+  const output = [];
+  array.forEach((subArr) => {
+    start.push(subArr[0]);
+    end.push(subArr[1]);
+  });
+  start.sort((a, b) => a - b);
+  end.sort((a, b) => a - b);
+  for (let i = 0; i < start.length; ++i) {
+    const tempArr = [];
+    tempArr.push(start[i]);
+    if (end[i] < start[i + 1]) {
+      tempArr.push(end[i]);
+      output.push(tempArr);
+    }
+    if (end[i] >= start[i + 1]) {
+      tempArr.push(end[i + 1]);
+      output.push(tempArr);
+      i++;
+    }
+  }
+  return output;
 }
+
+// test Data
+// var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
+// console.log(mergeRanges(times), 'to = [[0, 1], [3, 8], [9, 12]]')
 
 module.exports = mergeRanges;
