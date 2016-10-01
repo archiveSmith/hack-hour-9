0a44c18c184e1db2dd7981722272b08e3cdaad1b
Weylin Wagnon <weylinwagnon@gmail.com>
2016-10-01 11:34:44 -0700

    finished

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,34 @@
 
 
 function mergeRanges(array) {
+  array.sort((a, b) => a[0] - b[0]);
+  let len = array.length,
+    cur = array;
+  while (true) {
+    cur = combine(cur);
+    if (cur.length === len) break;
+    len = cur.length;
+  }
+  return cur;
 
+  function combine(arr) {
+    const result = [];
+    for (let i = 0; i < arr.length - 1; i++) {
+      result.concat(combineRanges(arr[i], arr[i + 1]));
+    }
+    return result;
+  }
+
+
+  function combineRanges(r1, r2) {
+    //[1,5], [0,8]
+    if (((r1[0] >= r2[0]) && (r1[0] <= r2[1])) || ((r1[1] <= r2[1]) && (r1[1] >= r2[0]))) {
+      return [Math.min(r1[0], r2[0]), Math.max(r1[1], r2[1])];
+    }
+    else {
+      return r1, r2;
+    }
+  }
 }
 
 module.exports = mergeRanges;
