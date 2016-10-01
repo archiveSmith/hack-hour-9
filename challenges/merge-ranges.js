9cb93724acc0cd9b1977915eb11aed4781fee301
Simon Situ <ssitu001@gmail.com>
2016-09-30 21:06:43 -0700

    complete mergeRanges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,30 @@
 
 
 function mergeRanges(array) {
+	
+	let merged = [];
+	let curr;
 
+	array = array.sort((a, b) => {
+		return a[0] > b[0];
+	});
+
+	merged.push(array[0]);
+	
+	for (let i = 1; i < array.length; i++) {
+		curr = array[i];
+		if(curr[0] - merged[merged.length-1][1] > 0) {
+			merged.push(curr);
+		} else {
+			merged[merged.length-1][1] = curr[1];
+		}
+	}
+	
+	return merged;
+	
 }
 
+
+var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]];
+//var times = [[3,5]];
 module.exports = mergeRanges;
