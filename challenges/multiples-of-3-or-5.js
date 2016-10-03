326be72cb145232fa507b40550d2a319ad3b49a1
Simon Situ <ssitu001@gmail.com>
2016-10-03 09:40:58 -0700

    complete mult3or5

---
+++ challenges/multiples-of-3-or-5.js
@@ -0,0 +1,41 @@
+use strict';
+// If we list all the natural numbers below 10 that are multiples of 3 or 5,
+// we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// write a function that will find the sum of all the multiples of 3 or 5
+// below 1000 and return that sum.
+
+function sumMultiples3Or5Below1000() {
+  let sum = 0;
+
+  for (let i = 3; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+
+  return sum; 
+}
+
+sumMultiples3Or5Below1000()
+// extension make it dynamic function that takes input x,y,z
+// and returns the sum of multiples of x and y below z
+function sumMultiplesXOrYBelowZ(x, y, z) {
+  let sum = 0;
+  let lesser = Math.min(x,y);
+  for (let i = lesser; i < z; i++) {
+    if (i % x === 0 || i % y === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+sumMultiplesXOrYBelowZ(3, 5, 1000)
+
+const objectToExport = {
+  sumMultiples3Or5Below1000,
+  sumMultiplesXOrYBelowZ,
+};
+
+module.exports = objectToExport;
