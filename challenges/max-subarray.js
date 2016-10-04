6c94f6f0b5d9cf8dab73336661bad5f42e943733
David Petri <petridw@gmail.com>
2016-10-04 09:10:41 -0700

    create max-subarray.js

---
+++ challenges/max-subarray.js
@@ -0,0 +1,14 @@
+/* You are given an array of integers with both positive and negative numbers. Write a function to
+ * find the maximum sum of all subarrays. A subarray is a section of consecutive elements from the
+ * original array. The whole array can be considered a sub array of itself.
+ *
+ * For example: maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]) -> 18 from [3, 10, -4, 7, 2]
+ *              maxSubarray([15,20,-5,10])  -> 40
+ *
+ */
+
+function maxSubarray(arr) {
+
+}
+
+module.exports = maxSubarray;
