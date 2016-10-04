fa551995e51801225425fc031864f0fb815790df
Jamey Stiling <jastiling@gmail.com>
2016-10-04 09:10:57 -0700

    fix merging

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-use 'strict';
-=======
 'use strict';
->>>>>>> 97ceac78af93ca886d0a423d2187fe00130714f9
 // If we list all the natural numbers below 10 that are multiples of 3 or 5,
 // we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
@@ -11,13 +7,9 @@ use 'strict';
 
 function sumMultiples3Or5Below1000() {
   let sum = 0;
-<<<<<<< HEAD
   for (let i = 0; i < 1000; i++) {
     if (i % 5 === 0 || i % 3 === 0) sum += i;
   }
-=======
-
->>>>>>> 97ceac78af93ca886d0a423d2187fe00130714f9
   return sum;
 }
 
@@ -25,13 +17,9 @@ function sumMultiples3Or5Below1000() {
 // and returns the sum of multiples of x and y below z
 function sumMultiplesXOrYBelowZ(x, y, z) {
   let sum = 0;
-<<<<<<< HEAD
   for (let i = 0; i < z; i++) {
     if (i % x === 0 || i % y === 0) sum += i;
   }
-=======
-  
->>>>>>> 97ceac78af93ca886d0a423d2187fe00130714f9
   return sum;
 }
 
