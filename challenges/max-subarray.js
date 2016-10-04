aacd37f131f111f9c559ad13dd58bd8f27ef414e
Daniel King <daniel.oliver.king@gmail.com>
2016-10-04 09:32:40 -0700

    Brute force max-subarray

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,35 @@
  */
 
 function maxSubarray(arr) {
+  return Math.max(...range(1, arr.length).map(length => {
+    return Math.max(...subarrays(arr, length).map(subarray => {
+      return subarray.reduce((a, b) => a + b, 0);
+    }));
+  }));
 
+  function subarrays(arr, length) {
+    const result = [];
+
+    for (let i = 0; i < arr.length - length + 1; i++) {
+      result.push(arr.slice(i, i + length));
+    }
+
+    return result;
+  }
+
+
+  function range(start, stop) {
+    const result = [];
+
+    for (let i = start; i <= stop; i++) result.push(i);
+
+    return result;
+  }
 }
+
+
+console.log(maxSubarray([1, 2, 3, 4, 5]));
+console.log(maxSubarray([1, 2, 3, 4, -5]));
+console.log(maxSubarray([1, 2, -5, 3, 4]));
 
 module.exports = maxSubarray;
