35dea543005d4bffd268086d11dd539233d8b261
Michael Kulinski <me@michaelkulinski.com>
2016-10-04 09:09:33 -0700

    idk

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -1,5 +1,6 @@
 /**
- * Write a function mergeRanges that takes an array of meeting time ranges and returns an array of condensed ranges.
+ * Write a function mergeRanges that takes an array of meeting time ranges and returns an
+ * array of condensed ranges.
  *
  * Example:
  * var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]
@@ -9,9 +10,33 @@
  * Do not assume the ranges are in order
  */
 
+//grab each array inside of the array
+//sort from smallest to largest range
+// for each array check the first number compared to all the other numbers
 
 function mergeRanges(array) {
+  let sortedArr = array;
+  let newArr = [];
+
+  sortedArr.sort((item1, item2) => {
+    return item1[0] - item2[0];
+  })
+
+  for (let i = 0; i < sortedArr.length - 1; i++) {
+    if (sortedArr[i][1] >= sortedArr[i + 1][0]) {
+      let tempArr = [sortedArr[i][0], sortedArr[i + 1][1]];
+      newArr.push(tempArr);
+    } else {
+      newArr.push(sortedArr[i]);
+    }
+  }
+  return sortedArr;
 
 }
+
+const times = [[3, 5], [0, 1], [4, 8], [10, 12], [9, 10]];
+console.log(times)
+
+console.log(mergeRanges(times));
 
 module.exports = mergeRanges;
--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -15,9 +15,5 @@ function subsetSum(array, target) {
   return subsetSum(array.slice(1), target - array[0]) || subsetSum(array.slice(1), target);
 }
 
-console.log(subsetSum([3, 7, 4, 2], 5)); //1,2 === 7,4
-console.log(subsetSum([3, 34, 4, 12, 5, 12], 32));
-console.log(subsetSum([8, 2, 4, 12], 13));
-console.log(subsetSum([8, -2, 1, -3], 6));
 
 module.exports = subsetSum;
