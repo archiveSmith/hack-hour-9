1b303551e9f7d45b8b225a6ea298c8129aa9ebf0
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-10-05 09:28:35 -0700

    finished anagrams hack hour

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,30 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
+function anagrams(str) {
+  var arr = str.split(''),
+    len = arr.length,
+    perms = [],
+    rest,
+    picked,
+    restPerms,
+    next;
 
+  if (len === 0)
+    return [str];
+
+  for (var i = 0; i < len; i++) {
+    rest = Object.create(arr);
+    picked = rest.splice(i, 1);
+
+    restPerms = anagrams(rest.join(''));
+
+    for (var j = 0, jLen = restPerms.length; j < jLen; j++) {
+      next = picked.concat(restPerms[j]);
+      perms.push(next.join(''));
+    }
+  }
+  return perms;
 }
 
 module.exports = anagrams;
