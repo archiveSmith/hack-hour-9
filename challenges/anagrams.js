ba42714c2c2237f5f7826e62b7d3f4f6582b52f1
Todd Chisom <hello@toddchisom.me>
2016-10-06 14:14:29 -0700

    anagramalanamanoman

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,27 @@
   */
 
 function anagrams(string) {
-
-}
-
+  	const results = makeAnagrams("", string);
+  	const noRepeats = results.reduce(function(obj, val, i) {
+  		    obj[val] = 0;
+  		    return obj;
+  	    }, {});
+  
+  	return Object.keys(noRepeats);
+  }
+  
+  function makeAnagrams(head, rest){
+      const intermediate = [];
+      if (rest.length === 0) {
+          intermediate.push(head);
+      }
+      let prefix;
+      let suffix;
+      for (let i = 0; i < rest.length; i++) {
+          prefix = rest.slice(0, i);
+          suffix = rest.slice(i+1);
+          intermediate = intermediate.concat(makeAnagrams(head + rest.charAt(i), prefix + suffix));
+      }
+      return intermediate;
+  }
 module.exports = anagrams;
