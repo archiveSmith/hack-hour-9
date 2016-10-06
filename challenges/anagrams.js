5416c295b3767a210543b896bedc0ab5c74f472b
sneakykate <sneakyk8@gmail.com>
2016-10-06 08:39:47 -0700

    anagrams not yet solved

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -11,9 +11,33 @@
   * var result = anagrams('abc');
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
-
 function anagrams(string) {
+  //something like Rock Paper Scissors except there are more than 3 options.
+  let arr = string.split("");
+  let len = string.length;
+  let combos = 1;
+  //# of possible combos for length of array.
+  for (let j = len; j > 0 ; j--){
+	combos *= j;
+	}
+	let outer = new Array(combos);
+	
+  for(let i = 0; i < (combos/len); i++){
+  	let inner = '';
+  	inner += arr[i];
+  	outer[i]=(inner);
+  }
+  console.log(outer)
+  let newstring = "";
+  if (string.length === 1){
+	//base case
+  }
+  
+  //something recursive involving each segment of the word and the combos that are possible with the remaining letters.
 
 }
+
+anagrams("pilot")
+
 
 module.exports = anagrams;
