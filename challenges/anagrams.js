b311ef46f9d51b0b263387d02e1bf8bc863da6ab
Dana Tran <dana.l.tran@gmail.com>
2016-10-06 09:39:28 -0700

    need to redo anagrams later

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,36 @@
   */
 
 function anagrams(string) {
-
+	var final = [];
+	var total = 1;
+	for (var i = string.length; i > 0; i--){
+		total *= i;
+	}
+	console.log('total', total/string.length);
+	for (var j = 0; j < total/string.length; j++){
+		if(final.length === 0){
+			final.push(string);
+		} else {
+			var temp = string[1];
+			
+			final.push();
+		}
+	}
+	// console.log('final', final);
+	var splitted = string.split('');
+	console.log(splitted);
+	
+	for (var k = 0; k < final.length; k++){
+		
+	}
+	//get length of string
+	//hold first index in place
+	//remove the letter you're holding from the splitted string
+	//loop through split string starting at the next index
+	//concatenate 
+	
+	
+	return final;
 }
 
 module.exports = anagrams;
