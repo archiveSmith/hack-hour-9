6979a8e546f8f9c3acb74ac17ee331dda9300d2c
jaycetan <jaycetan@gmail.com>
2016-10-05 19:19:39 -0700

    anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -7,13 +7,28 @@
  */
 
 /**
-  * example:
-  * var result = anagrams('abc');
-  * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
-  */
+ * example:
+ * var result = anagrams('abc');
+ * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+ */
 
 function anagrams(string) {
-
+  let str = string.split('');
+  let hash = {};
+  let output = [];
+  let perm;
+  let c;
+  // console.log(str);
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < str.length; j++) {
+      perm = str;
+      c = perm[j];
+      perm[j] = str[i];
+      perm[i] = c;
+      hash[perm.join('')] = true;
+    }
+  }
+  return Object.keys(hash);
 }
 
 module.exports = anagrams;
