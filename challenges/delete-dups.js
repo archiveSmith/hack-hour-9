f7e01a4fb1b4d128087c8900ed5fff820f163fff
Simon Situ <ssitu001@gmail.com>
2016-10-07 10:03:01 -0700

    complete delete dupes

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -12,8 +12,44 @@
 
 
 
-function deleteDups(head) {
-
+function Node(val) {
+	this.val = val;
+	this.next = null;
 }
+
+function deleteDups(head) {
+	let noDupes = [];
+	let newTail;
+	let newHead;
+	
+	let curr = head;
+	
+	//loop through linkedlist 
+	while(curr) {
+		//place values UNIQUE into an Array
+		if(noDupes.indexOf(curr.val) === -1) {
+			noDupes.push(curr.val)
+		}
+		curr = curr.next;
+	}
+
+	newHead = new Node(noDupes.shift());
+	newTail = newHead;
+	
+	while(noDupes.length) {
+		let curr = noDupes.shift();
+		newTail.next = new Node(curr);
+		newTail = newTail.next;
+	}
+
+	return newHead;
+}
+
+let linkedList = new Node(1);
+linkedList.next = new Node(2);
+linkedList.next.next = new Node(2);
+linkedList.next.next.next = new Node(3);
+linkedList.next.next.next.next = new Node(2);
+deleteDups(linkedList);
 
 module.exports = deleteDups;
