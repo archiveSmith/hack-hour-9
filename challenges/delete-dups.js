0e410fbdd21922039c9c58796a5854a85ffe855d
bryanyee <yee.bryan.c@gmail.com>
2016-10-07 09:47:41 -0700

    Partially complete delete-dups

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -11,9 +11,34 @@
  */
 
 
-
 function deleteDups(head) {
+  const values = [];
+  let curr = head;
 
+  while (curr) {
+    values.push(curr.value);
+    if (curr.next && values.indexOf(curr.next.value) !== -1) {
+      curr.next = curr.next.next;
+      curr = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+
+  return head;
 }
 
 module.exports = deleteDups;
+
+
+// function Node(value, next) {
+//   this.value = value;
+//   this.next = next;
+// }
+
+// let node3 = new Node(3);
+// let node2 = new Node(2, node3);
+// let node1 = new Node(1, node2);
+// let node0 = new Node(0, node1);
+
+// deleteDups(node0);
