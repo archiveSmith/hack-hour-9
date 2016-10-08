da53c949d3508a41f76d375dc7b808e3ccfcd0da
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-10-07 23:19:47 -0700

    fixed delete dups

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -13,7 +13,21 @@
 
 
 function deleteDups(head) {
+  if (!head) return null;
 
+  const cache = {};
+  cache[head.value] = true;
+
+  for (let it = head; it && it.next; it = it.next) {
+    if (cache[it.next.value]) {
+      it.next = it.next.next;
+    }
+    else {
+      cache[it.next.value] = true;
+    }
+  }
+
+  return head;
 }
 
 module.exports = deleteDups;
