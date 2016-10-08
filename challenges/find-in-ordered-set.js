3008ebbcc4e5663cb52130db6eaee1e2a79c5edb
Dana Tran <dana.l.tran@gmail.com>
2016-10-08 10:16:12 -0700

    completed find in ordered set hh

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -8,10 +8,24 @@ findInOrderedSet(nums, 4);  -> true
 findInOrderedSet(nums, 2);  -> false
 
  */
-
+//resource for future reference: http://stackoverflow.com/questions/9152890/what-would-cause-an-algorithm-to-have-olog-n-complexity
 
 function findInOrderedSet(arr, target) {
-
+	var mid;
+	if (target > arr[arr.length-1] || target < arr[0]){
+		return false;
+	}
+	while(arr.length){
+		mid = Math.floor(arr.length/2);
+		if(target === arr[mid]){
+			return true;
+		} else if (target < arr[mid]){
+			arr = arr.slice(0, mid);
+		} else {
+			arr = arr.slice(mid+1);	
+		}
+	}
+	return false;
 }
 
 
