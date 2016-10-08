a0105bbb9152dbb484ca7071fd375ace26df14b1
Weylin Wagnon <weylinwagnon@gmail.com>
2016-10-08 09:32:30 -0700

    finished

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,7 +11,10 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
-
+  if (!arr) return false;
+  if (arr[0] === target) return true;
+  len = len || arr.length;
+  return findInOrderedSet(arr.slice(0, len / 2), target) || findInOrderedSet(arr.slice(len / 2, len), target);
 }
 
 
