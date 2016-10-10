d51b7b938ee10b90990349efad7a4cd886d37cce
jaycetan <jaycetan@gmail.com>
2016-10-10 10:20:53 -0700

    insertion sort incomplete

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,31 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
+function insertionSort(arr) {
 
+  let count = 0;
 
-function insertionSort(array) {
+  while (count < arr.length - 1) {
+    let current = arr.splice(count, 1);
+    console.log(arr);
+    let insideCounter = count;
+    while (insideCounter > 0) {
+      if (arr[insideCounter] < current) {
+        arr.splice(insideCounter, 0, current);
+      } else {
+        insideCounter--
+      }
+    }
+    count++
+  }
 
 }
+
+var erray = [9, 7, 6, 8, 5, 3, 1, 2];
+
+// console.log( erray.splice(5,1));
+// console.log(erray);
+
+console.log(insertionSort(erray));
 
 module.exports = insertionSort;
