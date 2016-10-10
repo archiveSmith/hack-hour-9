0a2c9815fa5f23ebf0a2bd820f42f53e0adc6e64
Dlaosb <Dlaosb@gmail.com>
2016-10-09 23:54:51 -0700

    Refactored old Hack Hours

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,18 +11,34 @@
 
 
 function mergeRanges(array) {
-	array = array.sort(function(a,b) { return a[0]-b[0]; });
-	let result = []; 
-	result.push(array[0])
-	for(let i = 1; i < array.length; i++) {
-		if(result[result.length - 1][1] >= array[i][0]) {
-			result[result.length - 1][1] 
-			= Math.max(array[i][1], result[result.length - 1][1]);
-		} else {
-			result.push(array[i]);
-		}
-	}
-	return result; 
+	// array = array.sort(function(a,b) { return a[0]-b[0]; });
+	// let result = []; 
+	// result.push(array[0])
+	// for(let i = 1; i < array.length; i++) {
+	// 	if(result[result.length - 1][1] >= array[i][0]) {
+	// 		result[result.length - 1][1] 
+	// 		= Math.max(array[i][1], result[result.length - 1][1]);
+	// 	} else {
+	// 		result.push(array[i]);
+	// 	}
+	// }
+	// return result; 
+	array.sort(function(a,b){
+	    return a[0]-b[0];
+	  });
+	  var i = 0;
+	  while (i<array.length-1){
+	    if (array[i][1]>=array[i+1][0]){
+	      if (array[i][1]<array[i+1][1]){
+		array[i][1] = array[i+1][1];
+	      }
+	      array.splice(i+1,1);
+	    }
+	    else{
+	      i++;
+	    }
+	  }
+	  return array;
 }
 
 module.exports = mergeRanges;
