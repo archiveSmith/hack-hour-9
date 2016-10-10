30f0d2f10e1c258f7bf44523e66a97ac43df6851
Weylin Wagnon <weylinwagnon@gmail.com>
2016-10-10 09:44:36 -0700

    finsished

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,20 @@
 
 
 function insertionSort(array) {
-
+  let j, temp;
+  for (let i = 1, len = array.length; i < len; i++) {
+    j = i;
+    temp = array[i];
+    if (temp < array[i - 1]) {
+      while (j > 0) {
+      	if (array[j-1] <= temp) break;
+        array[j] = array[j-1];
+        j--;
+      }
+      array[j] = temp;
+    }
+  }
+  return array;
 }
 
 module.exports = insertionSort;
