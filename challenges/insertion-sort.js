971bf653111ffa0cda5850756b8612c6e2c36cb0
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-10-10 09:25:36 -0700

    completed insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,18 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
-
 function insertionSort(array) {
-
+  let length = array.length;
+  let temp, j;
+  for (let i = 1; i < length; ++i) {
+    temp = array[i];
+    j = i - 1;
+    while (j >= 0 && array[j] > temp) {
+      array[j + 1] = array[j--];
+    }
+    array[j + 1] = temp;
+  }
+  return array
 }
 
 module.exports = insertionSort;
