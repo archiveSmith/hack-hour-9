af529082a4be10b942a338fc31f58a0eb3d8c1b5
Jwookie55 <jwookie55@live.com>
2016-10-10 09:34:36 -0700

    finished insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -3,9 +3,21 @@
 // https://en.wikipedia.org/wiki/Insertion_sort
 
 
-
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i++) {
+    let j = i - 1;
+    const temp = array[i];
+    while (array[j] > temp) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = temp;
+  }
+  return array;
 }
+
+// // test
+// const testArr = [8, 2, 4, 3, 5, 1, 2];
+// console.log(insertionSort(testArr));
 
 module.exports = insertionSort;
