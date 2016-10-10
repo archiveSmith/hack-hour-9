e251a7b13b07416acc1803046f0ea65036f0ab8c
Daniel King <daniel.oliver.king@gmail.com>
2016-10-10 09:49:50 -0700

    Finish insertion sort (recursive solution)

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,23 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
+function insertionSort(array, start = 1) {
+  //Edge cases / Base case
+  if (!array || start >= array.length) return array;
 
+  // Recursive case
+  if (array[start] < array[start - 1]) {
+    const storedValue = array[start];
 
-function insertionSort(array) {
+    let i = start - 1;
+    for ( ; storedValue < array[i]; i--) {
+      array[i + 1] = array[i];
+    }
 
+    array[i + 1] = storedValue;
+  }
+
+  return insertionSort(array, start + 1);
 }
 
 module.exports = insertionSort;
