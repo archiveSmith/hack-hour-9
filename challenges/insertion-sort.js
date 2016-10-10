9305044452b5cbb92747e1510bee6563b3235a87
Derrick Ung <derrickrung@yahoo.com>
2016-10-10 09:51:38 -0700

    Completed insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -6,6 +6,17 @@
 
 function insertionSort(array) {
 
+	for(let i = 1; i < array.length; ++i) {
+		let temp = array[i];
+		let j = i - 1;
+		for(; array[j] > temp && j >= 0; --j) {
+				array[j + 1] = array[j];
+		}
+		array[j + 1] = temp;
+	}
+
+	return array;
+
 }
 
 module.exports = insertionSort;
