e1e0b813e8e91712ece8539e4510d874e1cc1bb8
Simon Situ <ssitu001@gmail.com>
2016-10-11 10:21:27 -0700

    complete uniquenumber, not in o(1) space

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -9,8 +9,20 @@
  * Complete the challenge in O(1) space
  *
  */
-function uniqueNumber(array) {
-
+function uniqueNumber(arr) {
+	
+	let unique = {};
+	
+	for(let i = 0; i < arr.length; i++) {
+		if(unique[arr[i]]) {
+			delete unique[arr[i]];
+		} else {
+			unique[arr[i]] = true;
+		}
+	}
+	return Number(Object.keys(unique)[0]);
 }
+
+uniqueNumber([1,2,1,3,3]);
 
 module.exports = uniqueNumber;
