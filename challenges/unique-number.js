ce246764177bac21b509652a5ea25f65688e3000
Jwookie55 <jwookie55@live.com>
2016-10-11 09:29:38 -0700

    finished unique-number

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -10,7 +10,18 @@
  *
  */
 function uniqueNumber(array) {
-
+  const count = {};
+  array.forEach((num) => {
+    count[num] ? count[num] += 1 : count[num] = 1;
+  });
+  for (let key in count) {
+    if (count[key] === 1) return key;
+  }
+  return false;
 }
+
+// tests
+console.log(uniqueNumber([1, 2, 1, 3, 3]), '= 2');
+console.log(uniqueNumber([1, 2, 2, 3, 3]), '= 1');
 
 module.exports = uniqueNumber;
