3879931ae1a1d64d68177b5e91e15a9795346438
sneakykate <sneakyk8@gmail.com>
2016-10-12 09:29:20 -0700

    too many loops

---
+++ challenges/get-all-products.js
@@ -0,0 +1,28 @@
+/* Accepts an array of integers and returns an array of all the possible products made by
+ * multiplying all but one number. In other words, find all the products of multiplying any
+ * array.length-1 numbers in the array.
+ *
+ * ex: getProducts([1, 7, 3, 4]); ->  [84, 12, 28, 21]
+ * this is done via:
+ * [7*3*4, 1*3*4, 1*7*4, 1*7*3]
+ *
+ * do not use division, becuase zero might be in the array and you cannot divide by zero
+ */
+function getAllProducts(array) {
+  let output = [];
+  if (array.length < 2){
+    return `Micky says "you're on your own with edge cases"`;
+  }
+  for (let i = 0; i < array.length; i++){
+    output.push(array.reduce(function(prev, curr, index){
+      if(index != i){
+        return curr * prev;
+      }
+      else {
+        return prev*1;
+      }
+    }))
+  }
+  return output;
+}
+module.exports = getAllProducts;
