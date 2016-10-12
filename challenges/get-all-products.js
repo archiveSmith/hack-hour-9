28bd23aa245aa1db0c7ed96677c35e0be592ff25
Jamey Stiling <jastiling@gmail.com>
2016-10-12 09:18:01 -0700

    getAllProducts

---
+++ challenges/get-all-products.js
@@ -0,0 +1,24 @@
+/* Accepts an array of integers and returns an array of all the possible products made by
+ * multiplying all but one number. In other words, find all the products of multiplying any
+ * array.length-1 numbers in the array.
+ *
+ * ex: getProducts([1, 7, 3, 4]); ->  [84, 12, 28, 21]
+ * this is done via:
+ * [7*3*4, 1*3*4, 1*7*4, 1*7*3]
+ *
+ * do not use division, becuase zero might be in the array and you cannot divide by zero
+ */
+function getAllProducts(array) {
+  let productArray = [];
+  for (let i = 0; i < array.length; i++) {
+    let mult = 1;
+    for (let j = 0; j < array.length; j++) {
+      if (j !== i) {
+        mult *= array[j]
+      }
+    }
+    productArray.push(mult)
+  }
+  return productArray;
+}
+module.exports = getAllProducts;
