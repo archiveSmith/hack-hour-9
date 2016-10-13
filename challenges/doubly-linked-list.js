cad20b2aed0b986d566930cb00bafd37bde982ea
Derrick Ung <derrickrung@yahoo.com>
2016-10-13 09:51:07 -0700

    Completed double linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -1,7 +1,6 @@
 /*
 Create a doubly linked list with an add and remove method
  */
-
 function LinkedList() {
   this.head = null;
   this.tail = null;
@@ -17,14 +16,41 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+	let temp = new Node(val)
+  if(!this.head || !this.tail) {
+  	this.head = temp;
+  	this.tail = temp;
+  } else {
+  	this.tail.next = temp;
+  	temp.prev = this.tail;
+  	this.tail = temp
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
+  let temp = this.head;
+  let past;
   
-};
+  while(temp) {
+  	if(temp.val === val) {
+  		if(temp == this.head) {
+  			this.head = temp.next;
+  			this.head.prev = null;
+  		} else if (temp == this.tail) {
+  			this.tail = temp.prev;
+  			this.tail.next = null
+  		} else {
+  		past = temp.prev;
+  		past.next = temp.next;
+  		temp.next.prev = past;
+  		break;
+  		}
+  	}
+  	temp = temp.next
+	}
+}
 
 module.exports = LinkedList;
