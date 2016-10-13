c10bb02cc5d540892f74c25b5c51f2a710849d50
Weylin Wagnon <weylinwagnon@gmail.com>
2016-10-13 09:40:44 -0700

    complete doubly linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,15 +16,47 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  
+LinkedList.prototype.add = function (val) {
+  if (this.tail) {
+    this.tail.next = new Node(val)
+    this.tail.next.prev = this.tail;
+    this.tail = this.tail.next
+  }
+  if (!this.head) {
+    this.head = new Node(val);
+    this.tail = this.head;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
-  
+LinkedList.prototype.remove = function (val) {
+  let cur = this.head;
+  while (cur) {
+    if (cur.val === val) {
+      if (!cur.prev) {
+        this.head = cur.next;
+        if (cur.next) {
+        	cur.next.prev = null;
+        } else {
+        	this.tail = null
+        }
+        return true
+      } else {
+        cur.prev.next = cur.next;
+        if (cur.next)  {
+        	cur.next.prev = cur.prev;
+        } else {
+        	this.tail = cur.prev;
+        }
+        
+        return true;
+      }
+    }
+    cur = cur.next
+  }
+  return false;
 };
 
 module.exports = LinkedList;
