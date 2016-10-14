c326a54b2a114d92c7dc86e4f000d4db550680f1
Jamey Stiling <jastiling@gmail.com>
2016-10-14 11:24:17 -0700

    lets see circle country

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -8,26 +8,19 @@
  *
  * do not use division, becuase zero might be in the array and you cannot divide by zero
  */
-// <<<<<<< HEAD
-// function getAllProducts(array) {
-//   if (array.length === 0) return [0];
-//   let productArray = [];
-//   for (let i = 0; i < array.length; i++) {
-//     let mult = 1;
-//     for (let j = 0; j < array.length; j++) {
-//       if (j !== i) {
-//         mult *= array[j]
-//       }
-//     }
-//     productArray.push(mult)
-//   }
-//   return productArray;
-// }
-// =======
+function getAllProducts(array) {
+  if (array.length === 0) return [0];
+  let productArray = [];
+  for (let i = 0; i < array.length; i++) {
+    let mult = 1;
+    for (let j = 0; j < array.length; j++) {
+      if (j !== i) {
+        mult *= array[j]
+      }
+    }
+    productArray.push(mult)
+  }
+  return productArray;
+}
 
-// function getAllProducts(array) {
-
-// }
-
-// >>>>>>> 72cb4a8ff4445f50af34aba53fcf550f3e3debb9
 module.exports = getAllProducts;
