7dcc4b9bfae791d806ef8c4cb49d9051091218e1
Jamey Stiling <jastiling@gmail.com>
2016-10-15 09:21:03 -0700

    bubbles

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,18 @@
 
 
 function bubbleSort(array) {
-
+    for (var i = (array.length - 1); i >= 0; i--) {
+        //Number of passes
+        for (var j = (array.length - i); j > 0; j--) {
+            //Compare the adjacent positions
+            if (array[j] < array[j - 1]) {
+                //Swap the numbers
+                let tmp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = tmp;
+            }
+        }
+    }
 }
 
 module.exports = bubbleSort;
