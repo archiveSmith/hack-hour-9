672461c381a86a0d76d34dad4c6cf12cb299b938
bryanyee <yee.bryan.c@gmail.com>
2016-10-15 09:45:10 -0700

    Complete bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -3,9 +3,25 @@
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 
-
 function bubbleSort(array) {
+  let swapped = false;
 
+  for (let i = 0; i < array.length; i++) {
+    swapped = false;
+
+    for (let j = 0; j < array.length - i; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        swapped = true;
+      }
+    }
+
+    if (!swapped) return array;
+  }
+
+  return array;
 }
 
 module.exports = bubbleSort;
