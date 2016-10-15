8976eb0db17577c43ae96b12cdef3fe9aae4d1d7
Simon Situ <ssitu001@gmail.com>
2016-10-15 09:41:11 -0700

    complete bubblesort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -6,6 +6,23 @@
 
 function bubbleSort(array) {
 
+  let swapped = false;
+
+  for (let i = 0; i < array.length; i++) {
+    swapped = false;
+    for (let j = 0; j < array.length-i; j++) {
+      if(array[j] > array[j+1]) {
+      let temp = array[j];
+      array[j] = array[j+1];
+      array[j+1] = temp;
+      flag = true;
+      }
+    }
+    if(!flag) {
+      return array;
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
