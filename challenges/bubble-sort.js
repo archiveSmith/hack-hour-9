f7b823ad153c6b4780f3b62352ec5db37bc5a23d
Todd Chisom <hello@toddchisom.me>
2016-10-15 10:24:07 -0700

    Sorting the bubble way

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,23 @@
 
 
 function bubbleSort(array) {
+  let swapped = false;
 
+  for (let i = 0; i < array.length; i++) {
+    swapped = false;
+    for (let j = 0; j < array.length-i; j++) {
+      if(array[j] > array[j+1]) {
+      let temp = array[j];
+      array[j] = array[j+1];
+      array[j+1] = temp;
+      swapped = true;
+      }
+    }
+    if(!swapped) {
+      return array;
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
