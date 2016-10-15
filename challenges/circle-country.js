554e3bef0a8202bd97036d3d2ced93ed2cf7b461
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-10-15 01:08:02 -0700

    finished circle country

--- challenges/circle-country.js
+++ challenges/circle-country.js
@@ -23,7 +23,20 @@
  */
 
 function circleCountry(x, y, r, start_x, start_y, end_x, end_y) {
-
+  let borders = 0;
+  for (let i = 0; i < x.length; i++) {
+    if (Math.sqrt(Math.pow((x[i] - start_x), 2) + Math.pow((y[i] - start_y), 2)) < r) {
+      borders++;
+      if (Math.sqrt(Math.pow((x[i] - end_x), 2) + Math.pow((y[i] - end_y), 2)) < r) {
+        borders--;
+      }
+    }
+    else {
+      if (Math.sqrt(Math.pow((x[i] - end_x), 2) + Math.pow((y[i] - end_y), 2)) < r) {
+        borders++;
+      }
+    }
+  }
+  return borders;
 }
-
 module.exports = circleCountry;
