bb81e33745549aa8a1c9b6a7ae18d16959cc3695
Michael Kulinski <me@michaelkulinski.com>
2016-10-15 09:34:30 -0700

    might help if my function returned something

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -2,18 +2,17 @@
 
 // https://en.wikipedia.org/wiki/Bubble_sort
 
-
-
 function bubbleSort(array) {
   for (let i = 0; i < array.length; i++) {
-     for (let j = 0; j < array.length; j++) {
-       if (array[j] < array[j+1]) {
-         const tempNum = array[j];
-         array[j] = array[j+1];
-         array[j+1] = tempNum;
-       }
-     }
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] < array[j + 1]) {
+        const tempNum = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tempNum;
+      }
+    }
   }
+  return array;
 }
 
 module.exports = bubbleSort;
