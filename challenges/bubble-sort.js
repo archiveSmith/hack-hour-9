7e1c93a0ab2fe2cc3a10f08f330f0e60cea4c45d
Weylin Wagnon <weylinwagnon@gmail.com>
2016-10-15 09:34:33 -0700

    finished without temp variable

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,22 @@
 
 
 function bubbleSort(array) {
-
+  if (!Array.isArray(array) || array.length <= 1) return array;
+  let swapped = true,
+    i = 0,
+    len = array.length;
+  while (moved) {
+    swapped = false;
+    for (i = 0; i < len - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        array[i] = array[i] ^ array[i + 1];
+        array[i + 1] = array[i] ^ array[i + 1];
+        array[i] = array[i] ^ array[i + 1];
+        swapped = true;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
