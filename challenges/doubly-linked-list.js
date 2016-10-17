2f087314ac92adc2d042ae930cdef41769f86fd9
shawn-southwell <shawn.southwel@gmail.com>
2016-10-16 23:43:40 -0700

    finished doubly linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,15 +16,33 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  
-};
+LinkedList.prototype.add = function(val){
+	const newNode = new Node(val);
+	if (!this.head){
+		this.head = this.tail = newNode;
+	} else {
+		newNode.prev = this.tail;
+		this.tail.next = this.tail = newNode;
+	}
+}
 
-/*
-Removes the first node with the inputted value
- */
-LinkedList.prototype.remove = function(val) {
-  
-};
+LinkedList.prototype.remove = function(val){
+	let cur = this.head;
+	while(cur){
+		if (cur.val === val && cur.prev && cur.next){
+			cur.next.prev = cur.prev;
+			cur.prev.next = cur.next;
+		}
+		if (!cur.prev && cur.val === val){
+			cur.next.prev = null;
+			this.head = cur.next;
+		}
+		if(!cur.next && cur.val === val){
+			this.tail.next = this.tail = cur.prev;
+			cur.prev.next = null;
+		}
+		cur = cur.next;
+	}
+}
 
 module.exports = LinkedList;
