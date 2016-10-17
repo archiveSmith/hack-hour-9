0b5803d3abfeb3d767fea6176c33ad40fdc64a34
shawn-southwell <shawn.southwel@gmail.com>
2016-10-16 23:48:45 -0700

    finish bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -4,8 +4,16 @@
 
 
 
-function bubbleSort(array) {
-
+function bubbleSort(arr) {
+	for(let i = 0; i < arr.length; i++){
+		for(let j = 0; j < arr.length - i; j++){
+			if(arr[j] > arr[j+1]){
+				let temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+	return arr;
 }
-
 module.exports = bubbleSort;
