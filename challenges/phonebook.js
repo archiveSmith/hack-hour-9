18045a587f5d392fec5d77bbbbcf9f3bc336cbfc
Simon Situ <ssitu001@gmail.com>
2016-10-17 09:51:28 -0700

    complete phonebook

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -19,15 +19,44 @@
 */
 
 function findName(jazbook, name) {
-  
-  return number;
+	let phonenumber;
+	jazbook.filter( subArray => {
+	  	if(name === subArray[0]) {
+	  		phonenumber =  subArray[1];
+	  	}
+	  });
+	 return phonenumber ? phonenumber : false;
 }
 
 function makeJazBookIntoARealPhoneBookObject(jazbook){
-
-  return phonebook;
+	this.phoneBook = {};
+	jazbook.forEach( subArray => 
+		this.phoneBook[subArray[0]] = subArray[1]);
 }
 
+makeJazBookIntoARealPhoneBookObject.prototype.add = function(name, phonenumber) {
+	if(name && phonenumber) {
+		this.phoneBook[name] = phonenumber;
+	}
+}
+
+makeJazBookIntoARealPhoneBookObject.prototype.remove = function(name) {
+	delete this.phoneBook[name];
+}
+//add
+//remove
+
+//you can call them ‘add’ and ‘remove’
+//the add method just takes info for one entry as params
+//the remove takes in the name
+
+var jazbook = [
+     ['alex','301-844-3421'],
+     ['jae','301-844-1211'],
+     ['david','301-844-0978'],
+     ['travis','301-844-8505'],
+    ['jasmine','1800-974-4539']
+  ];
 
 var objectToExport = {
   findName: findName,
