8c9f9b5b9fa4768b2b8ceafb100badae35fedf8e
bryanyee <yee.bryan.c@gmail.com>
2016-10-17 09:43:07 -0700

    Complete phonebook

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -12,26 +12,58 @@
 *
 * jazbooks are not always sorted...
 *
-* Develop a function that takes in a jazbook and a name, searches through the jazbook and 
+* Develop a function that takes in a jazbook and a name, searches through the jazbook and
 * returns the persons phone number. If the person does not exists, return false.
 *
 * How efficient can you make this?
 */
 
 function findName(jazbook, name) {
-  
-  return number;
+  const phonebook = new makeJazBookIntoARealPhoneBookObject(jazbook);
+
+  return phonebook.getNumber(name);
 }
 
-function makeJazBookIntoARealPhoneBookObject(jazbook){
+function makeJazBookIntoARealPhoneBookObject(jazbook) {
+  this.phonebook = {};
 
-  return phonebook;
+  jazbook.forEach((person) => {
+    this.phonebook[person[0]] = person[1];
+  });
 }
 
-
-var objectToExport = {
-  findName: findName,
-  makeJazBookIntoARealPhoneBookObject: makeJazBookIntoARealPhoneBookObject
+makeJazBookIntoARealPhoneBookObject.prototype.getNumber = function(name) {
+  return this.phonebook[name] || false;
 };
 
-module.exports=objectToExport;
+makeJazBookIntoARealPhoneBookObject.prototype.add = function(name, phone) {
+  this.phonebook[name] = phone;
+};
+
+makeJazBookIntoARealPhoneBookObject.prototype.remove = function(name) {
+  if (this.phonebook[name]) {
+    const removed = this.phonebook[name];
+    delete this.phonebook[name];
+    return removed;
+  } else {
+    return undefined;
+  }
+};
+
+const objectToExport = {
+  findName,
+  makeJazBookIntoARealPhoneBookObject,
+};
+
+module.exports = objectToExport;
+
+
+// const jazbook = [
+//      ['alex', '301-844-3421'],
+//      ['jae', '301-844-1211'],
+//      ['david', '301-844-0978'],
+//      ['travis', '301-844-8505'],
+//      ['jasmine', '1800-974-4539'],
+// ];
+
+// const number = findName(jazbook, 'jae');
