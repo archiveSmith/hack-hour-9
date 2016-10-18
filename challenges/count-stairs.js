c7a1757d2c931e282d0492e9dce1395e9b845c2a
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-10-18 09:45:09 -0700

    finish count stairs

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -2,20 +2,16 @@
  * a function to calculate how many different ways you can go up the flight of stairs.
  */
 
-function countStairs(n) {
-	let least = Math.floor(n/2);
-	let possible = 0
-	for (var i = 0; i < least; i++){
-		possible += fact(n-least*i);
-	}
-	return possible
+function fib(n) {
+  if (n <= 1) {
+    return n;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  }
 }
 
-function fact(x) {
-   if(x==0) {
-      return 1;
-   }
-   return x * fact(x-1);
+function countStairs(n) {
+  return fib(n + 1);
 }
 
 module.exports = countStairs;
