3c5d25f57180743530944aab3abf2ee0e9f7678f
Dana Tran <dana.l.tran@gmail.com>
2016-10-17 22:05:28 -0700

    completed phonebook

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -6,7 +6,7 @@
 *     ['alex','301-844-3421'],
 *     ['jae','301-844-1211'],
 *     ['david','301-844-0978'],
-*     ['travis','301-844-8505']
+*     ['travis','301-844-8505'],
 *     ['jasmine','1800-974-4539'],
 *   ];
 *
@@ -19,13 +19,34 @@
 */
 
 function findName(jazbook, name) {
-  
-  return number;
+  //loop through jazbook, if array[0] === name, return array[1]
+  //if person doesn't exist, return false
+  for (var i = 0; i < jazbook.length; i++){
+  	if (jazbook[i][0] === name){
+  		return jazbook[i][1];
+  	}
+  }
+  return false;
 }
 
 function makeJazBookIntoARealPhoneBookObject(jazbook){
+  
+  this.storage = {};
 
-  return phonebook;
+  this.add = function(name, number){
+    this.storage[name] = number;
+  }
+
+  this.remove = function(name){
+    if (this.storage[name]){
+      delete this.storage[name];
+    }
+  }
+  
+  for (var j = 0; j < jazbook.length; j++){
+    this.add(jazbook[j][0],jazbook[j][1]);
+  }
+  return this.storage;
 }
 
 
