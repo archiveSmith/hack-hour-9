3a613233f9a4a876ab14809d7b216916108ffcb6
Jose Neves <joc.neves@gmail.com>
2016-10-18 14:33:43 -0200

    counting stairs done

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -3,7 +3,11 @@
  */
 
 function countStairs(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  if (n === 2) return 2;
 
+  return countStairs(n - 1) + countStairs(n - 2);   
 }
 
 module.exports = countStairs;
