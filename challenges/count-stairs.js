f545886831413f0e8952b721896f61070136a203
bryanyee <yee.bryan.c@gmail.com>
2016-10-18 13:15:35 -0700

    Complete count-stairs

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -2,8 +2,56 @@
  * a function to calculate how many different ways you can go up the flight of stairs.
  */
 
+'use strict';
+
 function countStairs(n) {
 
+  function innerCount(remainingSteps) {
+    if (n === 0 || remainingSteps === 0) return 1;
+    if (remainingSteps < 0) return 0;
+    return (remainingSteps - 1) + (remainingSteps - 2);
+  }
+
+  return innerCount(n, n);
 }
 
+// let count1 = countStairs(1);
+// let count2 = countStairs(2);
+// let count3 = countStairs(3);
+// let count4 = countStairs(4);
+
+
 module.exports = countStairs;
+
+
+
+// function padWithZeros(string, length) {
+//   let nString = string;
+
+//   while (nString.length < length) {
+//     nString = '0' + nString;
+//   }
+
+//   return nString;
+// }
+
+// function countStairs(n) {
+//   let count = 0;
+//   const binCombos = Math.pow(2, n);
+
+//   if (!n) return 1;
+
+//   for (let i = 0; i < binCombos; i++) {
+//     const bin = padWithZeros(i.toString(2), n);
+//     let steps = 0;
+
+//     for (let j = 0; j < bin.length; j++) {
+//       if (bin[j] === '0') steps += 1;
+//       else steps += 2;
+//     }
+
+//     if (steps === n) count++;
+//   }
+
+//   return count;
+// }
