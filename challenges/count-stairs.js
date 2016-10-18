49dd006962a602bca18897cfa2db8988b774f4e3
Michael Kulinski <me@michaelkulinski.com>
2016-10-18 09:35:39 -0700

    no idea

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -7,10 +7,11 @@ function countStairs(n) {
     return 1;
   } else if (n < 0) {
     return 0;
-  } else {
+  } else if (n > 0) {
     return n;
   }
-  // return countStairs(n - 1) + countStairs(n - 2);
+
+  return countStairs(n - 1) + countStairs(n - 2);
 }
 
 console.log(countStairs(3));
