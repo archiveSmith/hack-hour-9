be3debd1c922f815f4be2784414b986b564c1267
William Barnes <barnesw91291@gmail.com>
2016-10-20 09:44:19 -0700

    constant time and space

--- challenges/knight-jumps.js
+++ challenges/knight-jumps.js
@@ -11,7 +11,39 @@
 // var str = "(4 5)"
 
 function knightjumps(str) {
-
+  let x = +str[1];
+  let y = +str[3];
+  if (x === 8) {
+    x -= 7;
+  } else if (x === 7) {
+    x -= 5;
+  } else if (x === 6) {
+    x -= 3;
+  } else if (x === 5) {
+    x -= 1;
+  }
+  if (y === 8) {
+    y -= 7;
+  } else if (y === 7) {
+    y -= 5;
+  } else if (y === 6) {
+    y -= 3;
+  } else if (y === 5) {
+    y -= 1;
+  }
+  let jumpCount = 0;
+  if (Math.pow((x * y), 2) === 1) {
+    jumpCount = 2;
+  } else if (Math.pow((x * y), 2) === 4) {
+    jumpCount = 3;
+  } else if (Math.pow((x * y), 2) === 9 || Math.pow((x * y), 2) === 16) {
+    jumpCount = 4;
+  } else if (Math.pow((x * y), 2) === 36 || Math.pow((x * y), 2) === 64) {
+    jumpCount = 6;
+  } else {
+    jumpCount = 8;
+  }
+  return jumpCount;
 }
 
 module.exports = knightjumps;
