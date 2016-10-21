8a77081b5bb30a83df7e44d5078df3582527a9c9
medic27 <jczhang@ucla.edu>
2016-10-21 10:10:26 -0700

    knight and pascal

--- challenges/knight-jumps.js
+++ challenges/knight-jumps.js
@@ -10,8 +10,28 @@
 //  example input:
 // var str = "(4 5)"
 
-function knightjumps(str) {
+// 8 maximum ways
+// two left and one up/down
+// two right and one up/down
+// two up and one left/right
+// two down and one left/right
+// do the math to see if any of those moves are legal, then add one to a counter for each legal move
 
+function knightjumps(str) {
+	let numMoves = 0;
+
+	//extract the coordinates
+	const position = [str[1], str[3]];
+	const [x, y] = position;
+
+	// helper function using destructuring
+	function isLegalPosition([x,y]) {
+		if ((x > 0 && x < 9) && (y > 0 && y < 9)) {
+			return true;
+		} else return false;
+	}
+
+	
 }
 
 module.exports = knightjumps;
--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -31,9 +31,22 @@
     [1,5,10,10,5,1]
   ]
 */
-
 function pascalTriangle(numRows) {
+  if (numRows === 1) return [[1]];
+  if (numRows === 0) return [];
+  if (numRows === 2) return [[1], [1,1]];
 
+  let prevTriangle = pascalTriangle(numRows - 1);
+  let maxRow = prevTriangle[prevTriangle.length - 1];
+  let newRow = [];
+
+  for (let i = 0; i < maxRow.length + 1; i++) {
+    if (i === maxRow.length) newRow.push(1);
+    else newRow.push(maxRow[i] + (maxRow[i - 1] || 0));
+  }
+  prevTriangle.push(newRow);
+
+  return prevTriangle;
 }
 
 module.exports = pascalTriangle;
