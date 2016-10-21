c9a37f2d64c258532804e51d176b83cd7cf6d4c4
Dana Tran <dana.l.tran@gmail.com>
2016-10-21 09:47:24 -0700

    completed pascal triangle

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -33,7 +33,34 @@
 */
 
 function pascalTriangle(numRows) {
-
+  var final = [];
+  var temp; //temp represents previous row
+  //return [] if numRows is not a number >= 1
+  if (typeof(numRows) !== 'number' || numRows < 1){
+    return final;
+  }
+  
+  for (var i = 0; i < numRows; i++){
+    var row = [1];  //each row starts with 1
+    if (i <= 1){
+      if (i === 0) {
+        temp = [1];
+        final.push(temp);
+      }
+      if (i === 1) {
+        temp = [1,1];
+        final.push(temp);
+      }
+    } else {
+      for (j = 0; j < temp.length-1; j++){
+        row.push(temp[j] + temp[j + 1]);    //loop through temp and add the 1st & 2nd, 2nd & 3rd, etc
+      }
+      temp = row;
+      row.push(1);  //each row ends with 1
+      final.push(row);
+    }
+  }
+  return final;
 }
 
 module.exports = pascalTriangle;
