8e9661dc3e4eb1232303f46d4d572617fa71c7f4
Daniel King <daniel.oliver.king@gmail.com>
2016-10-21 09:29:26 -0700

    Finished first version of pascal triangle

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -33,7 +33,29 @@
 */
 
 function pascalTriangle(numRows) {
+  if (typeof numRows !== 'number') return [];
+  if (isNaN(numRows)) return [];
+  if (numRows < 1) return [];
+  
+  if (numRows === 1) return [[1]];
+  
+  const result = pascalTriangle(numRows - 1);
+  const nextRow = result[result.length - 1].reduce((row, _, i, prevRow) => {
+    row.push(
+      prevRow[i + 1] ?
+        prevRow[i] + prevRow[i + 1] :
+        prevRow[i]
+    );
 
+    return row;
+  }, [1])
+  
+  result.push(nextRow);
+  return result;
+}
+
+for (let i = 1; i < 10; i++) {
+  console.log(pascalTriangle(i));
 }
 
 module.exports = pascalTriangle;
