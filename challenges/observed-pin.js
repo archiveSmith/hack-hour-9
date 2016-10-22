7ee451308ad2f63a480f7b1a696854a9e605d1a0
William Barnes <barnesw91291@gmail.com>
2016-10-22 10:02:59 -0700

    gettin sum pin numbas with terrible time complexity

--- challenges/observed-pin.js
+++ challenges/observed-pin.js
@@ -6,15 +6,7 @@ PIN he saw, when Robby entered it.
 
 The keypad has the following layout:
 
-┌───┬───┬───┐
-│ 1 │ 2 │ 3 │
-├───┼───┼───┤
-│ 4 │ 5 │ 6 │
-├───┼───┼───┤
-│ 7 │ 8 │ 9 │
-└───┼───┼───┘
-    │ 0 │
-    └───┘
+
 He noted the PIN 1357, but he also said, it is possible that each of the digits he saw could actually 
 be another adjacent digit (horizontally or vertically, but not diagonally). E.g. instead of the 1 it 
 could also be the 2 or 4. And instead of the 5 it could also be the 2, 4, 6 or 8.
@@ -39,12 +31,31 @@ expectations = {
 
 */
 
-
-
-
 function getPINs(observed) {
-
+  const adjacentOnKeypad = {
+    '1': [1, 2, 4],
+    '2': [1, 2, 3, 5],
+    '3': [2, 3, 6],
+    '4': [1, 4, 5, 7],
+    '5': [2, 4, 5, 6, 8],
+    '6': [3, 5, 6, 9],
+    '7': [4, 7, 8],
+    '8': [5, 7, 8, 9, 0],
+    '9': [6, 8, 9],
+    '0': [0, 8],
+  };
+  let perms = [''];
+  for (let i = 0; i < observed.length; i++) {
+    const adjacents = adjacentOnKeypad[observed[i]];
+    const currentPermutations = [];
+    for (let j = 0; j < adjacents.length; j++) {
+      for (let k = 0; k < perms.length; k++) {
+        currentPermutations.push(perms[k] + adjacents[j]);
+      }
+    }
+    perms = currentPermutations;
+  }
+  return perms;
 }
 
-
-module.exports = getPINs
+module.exports = getPINs;
