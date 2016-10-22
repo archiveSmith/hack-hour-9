2f6ee934234ed5455998e53b5d7603cf82577c8c
Weylin Wagnon <weylinwagnon@gmail.com>
2016-10-22 09:37:42 -0700

    finished

--- challenges/observed-pin.js
+++ challenges/observed-pin.js
@@ -43,8 +43,32 @@ expectations = {
 
 
 function getPINs(observed) {
-
+  const padObj = {
+    "1": ["1", "2", "4"],
+    "2": ["1", "2", "3", "5"],
+    "3": ["2", "3", "6"],
+    "4": ["1", "4", "5", "7"],
+    "5": ["2", "4", "5", "6", "8"],
+    "6": ["4", "5", "6", "9"],
+    "7": ["4", "7", "8"],
+    "8": ["0", "5", "7", "8", "9"],
+    "9": ["6", "8", "9"],
+    "0": ["8", "0"]
+  };
+  const results = [];
+  function posPin(ob, prev) {
+    if (prev.length === ob.length) {
+      results.push(prev);
+    } else {
+      for (let possible of padObj[ob[prev.length]]) {
+        posPin(ob, prev + possible);
+      }
+    }
+  }
+  posPin(observed, '');
+  return results;
 }
+
 
 
 module.exports = getPINs
