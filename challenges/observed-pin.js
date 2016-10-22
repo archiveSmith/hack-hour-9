1676eb5a6c050d0fac59202a3a5b78d8828ade89
bryanyee <yee.bryan.c@gmail.com>
2016-10-22 10:15:32 -0700

    Complete observed-pin

--- challenges/observed-pin.js
+++ challenges/observed-pin.js
@@ -15,18 +15,18 @@ The keypad has the following layout:
 └───┼───┼───┘
     │ 0 │
     └───┘
-He noted the PIN 1357, but he also said, it is possible that each of the digits he saw could actually 
-be another adjacent digit (horizontally or vertically, but not diagonally). E.g. instead of the 1 it 
+He noted the PIN 1357, but he also said, it is possible that each of the digits he saw could actually
+be another adjacent digit (horizontally or vertically, but not diagonally). E.g. instead of the 1 it
 could also be the 2 or 4. And instead of the 5 it could also be the 2, 4, 6 or 8.
 
-He also mentioned, he knows this kind of locks. You can enter an unlimited amount of wrong PINs, they 
-never finally lock the system or sound the alarm. That's why we can try out all possible (*) variations.
-
+He also mentioned, he knows this kind of lock. You can enter an unlimited amount of wrong PINs, they
+never lock the system or sound the alarm. That's why we can try out all possible (*) variations.
+jn
 * possible in sense of: the observed PIN itself and all variations considering the adjacent digits
 
-Can you help us to find all those variations? It would be nice to have a function, that returns an array 
-of all variations for an observed PIN with a length of 1 to 8 digits. We could name the function getPINs. 
-But please note that all PINs, the observed one and also the results, must be strings, because of 
+Can you help us find all those variations? It would be nice to have a function, that returns an array
+of all variations for an observed PIN with a length of 1 to 8 digits. We could name the function getPINs.
+But please note that all PINs, the observed one and also the results, must be strings, because of
 potentially leading '0's. Don't worry about the order of the array.
 
 Detective, we count on you!
@@ -36,15 +36,44 @@ expectations = {
   "11": ["11", "22", "44", "12", "21", "14", "41", "24", "42"],
   "369": ["339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368","638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239"],
 }
-
 */
 
 
-
-
 function getPINs(observed) {
+  if (observed.length === 1) return findAdjacentNums(observed);
 
+  const prevCombo = getPINs(observed.slice(0, observed.length - 1));
+  const currCombo = findAdjacentNums(observed[observed.length - 1]);
+  const results = [];
+
+  for (let i = 0; i < prevCombo.length; i++) {
+    const str = prevCombo[i];
+
+    for (let j = 0; j < currCombo.length; j++) {
+      results.push(str + currCombo[j]);
+    }
+  }
+
+  return results;
 }
 
+function findAdjacentNums(num) {
+  const table = {
+    '0': ['0', '8'],
+    '1': ['1', '2', '4'],
+    '2': ['1', '2', '3', '5'],
+    '3': ['2', '3', '6'],
+    '4': ['1', '4', '5', '7'],
+    '5': ['2', '4', '5', '6', '8'],
+    '6': ['3', '5', '6', '9'],
+    '7': ['4', '7', '8'],
+    '8': ['5', '7', '8', '9', '0'],
+    '9': ['6', '8', '9'],
+  };
 
-module.exports = getPINs
+  return table[num];
+}
+
+// getPINs("11");
+
+module.exports = getPINs;
