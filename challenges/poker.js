b461c8af39f7b566b648f5bf9b0ab4b6b05dc8b3
Simon Situ <ssitu001@gmail.com>
2016-10-24 09:43:13 -0700

    to be continued

--- challenges/poker.js
+++ challenges/poker.js
@@ -19,7 +19,44 @@
 */
 
 function poker(hand1, hand2) {
+	
+	let player1Hand = populateHand(hand1);
+	let player2Hand = populateHand(hand2);
+	
 
+	console.log(player1Hand)
+	console.log(player2Hand)
+	//handle high card
+	let highestCard = 0;
+	//iterate over p1 hands to find highestCard with 1 value
+	for(let card in player1Hand) {
+		if(player1Hand[card] === 1 && card > highestCard) {
+			highestCard = card;
+		}
+	}
+	for(let card in player2Hand) {
+		if(player2Hand[card] === 1 && card > highestCard) {
+			highestCard = card;
+		}
+	}
+	
+	console.log(highestCard)
+	
 }
+
+function populateHand(arr) {
+	let hand = {};
+	let length = arr.length;
+	for(var i = 0; i < length; i++) {
+		if(!hand[arr[i]]) {
+			hand[arr[i]] = 1;
+		} else {
+			hand[arr[i]]++;
+		}
+	}
+	return hand;
+}
+
+poker([1,2,3,4,5], [4,5,8,9,10])
 
 module.exports = poker;
