5959ea73434e73af57c5dca1e93dd1b5cd6bd359
William Barnes <barnesw91291@gmail.com>
2016-10-28 09:43:26 -0700

    recursive knapsack solution

--- challenges/knapsack-problem.js
+++ challenges/knapsack-problem.js
@@ -4,13 +4,23 @@
   Find the maximum value you can fit into your knapsack.
 
   e.g.
-  items = [{weight: 1, value : 3}, {weight: 2, value : 4}, {weight: 3, value : 5}];
+  const items = [{weight: 1, value : 3}, {weight: 2, value : 4}, {weight: 3, value : 5}];
   solveKnapsack(items, 3); // returns 7 (from items[0] and items[1])
   solveKnapsack(items, 5); // returns 9 (from items[1] and items[2])
 */
 
 function solveKnapsack(items, weightAvailable) {
-
-};
+  if (items.length === 0 || weightAvailable === 0) return 0;
+  if (items[0].weight > weightAvailable) {
+    return solveKnapsack(items.slice(1), weightAvailable);
+  }
+  const leftItems = items.slice(1);
+  const take = items[0].value + solveKnapsack(leftItems, weightAvailable - items[0].weight);
+  const leave = solveKnapsack(leftItems, weightAvailable);
+  if (take > leave) {
+    return take;
+  }
+  return leave;
+}
 
 module.exports = solveKnapsack;
