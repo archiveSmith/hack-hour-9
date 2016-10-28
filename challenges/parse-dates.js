3fdccab9baa497f4565e64536404ff1b3be3fe9b
William Barnes <barnesw91291@gmail.com>
2016-10-28 09:22:35 -0700

    date parser halfway done

--- challenges/parse-dates.js
+++ challenges/parse-dates.js
@@ -40,7 +40,19 @@
 // - if any part of the date string is missing then you can consider it an invalid date
 
 function parseDates(str) {
-  
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const words = str.split(' ');
+  if (words.length === 4) {
+    const month = months.indexOf(words[0]);
+    let day = +words[1].split('').filter((el) => !isNaN(el)).join('');
+    let time = words[2].split(':');
+    let hours = +time[0];
+    let minutes = +time[1];
+    return new Date(2016, month, day, hours, minutes).toString();
+  } else if (words.length === 3) {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  }
 }
+
 
 module.exports = parseDates;
