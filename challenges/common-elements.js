82ce6311efbd26f964fe946100696030181448c5
Daniel King <daniel.oliver.king@gmail.com>
2016-10-29 09:28:41 -0700

    Version 1 of common-element

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -12,8 +12,31 @@
 
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
-function commonElements(array1, array2, array3, array4){
-
+function commonElements(array1, array2, array3, array4) {
+  //return [array1, array2, array3, array4]
+  const result = array1
+    .filter(element => {
+      return (
+        array2.includes(element) &&
+        array3.includes(element) &&
+        array4.includes(element)
+      )
+    })
+    .reduce((acc, element) => {
+      if (!acc.includes(element)) acc.push(element);
+      return acc;
+    }, [])
+  
+  return result.length === 0 ?
+    "Nothing in Common!" :
+    result
 }
+
+var array1 = [1,4,6,7,99,2000,'dog','dog',99,1000];
+var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
+var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
+var array4 = ['ferret',12,12,45,9,66,77,78,2000];
+
+console.log(commonElements(array1, array2, array3, array4));
 
 module.exports = commonElements;
