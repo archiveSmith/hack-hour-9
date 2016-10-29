ac2947a12c6e2f0ea148f583815b67a598a3ba62
Jose Neves <joc.neves@gmail.com>
2016-10-29 10:17:07 -0700

    common elements done

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -3,17 +3,54 @@
 // duplicates are only counted once;
 // for example == given the following input
 
-  // var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
-  // var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
-  // var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
-  // var array4 = ['ferret',12,12,45,9,66,77,78,2000];
+  var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
+  var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
+  var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
+  var array4 = ['ferret',12,12,45,9,66,77,78,2000];
 
 // your output would be [ 12, 'ferret']
 
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
 function commonElements(array1, array2, array3, array4){
+  let storage = {};
+  let result = [];
 
+  array1.forEach((item) => {
+    if (!storage[item]) {
+      storage[item] = 1;
+    }
+  });
+
+  array2.forEach((item) => {
+    if (storage[item]) {
+      storage[item] = 2;
+    }
+  });
+
+  array3.forEach((item) => {
+    if (storage[item] === 2) {
+      storage[item] = 3;
+    }
+  });
+
+  array4.forEach((item) => {
+    if (storage[item] === 3) {
+      storage[item] = 4;
+    }
+  });  
+
+  for (let key in storage) {
+    if (storage[key] == 4) {
+      result.push(key);
+    }
+  }
+
+  if (result.length >= 1) {
+    return result;
+  } else {
+    return "Nothing in Common!";
+  }
 }
 
 module.exports = commonElements;
