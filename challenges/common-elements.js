7c731f5c46aa802fad5c4c1ba856fd04c0fbcfcd
Michael Kulinski <me@michaelkulinski.com>
2016-10-29 09:32:34 -0700

    might have gotten a hack hour?

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -3,10 +3,10 @@
 // duplicates are only counted once;
 // for example == given the following input
 
-  var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
-  var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
-  var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
-  var array4 = ['ferret',12,12,45,9,66,77,78,2000];
+  var array11 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
+  var array22  = [15,9,9,'ferret',9,26,12,12,'dog'];
+  var array33 = [23,12,12,77,'ferret',9,88,100,'dog'];
+  var array44 = ['ferret',12,12,45,9,66,77,78,2000];
 
 // your output would be [ 12, 'ferret']
 
@@ -15,8 +15,8 @@
 function commonElements(array1, array2, array3, array4){
   const allTheArrays = [array1, array2, array3, array4];
   const outArr = [];
-  for (let i = 0; i < allTheArrays; i++) {
-    allTheArrays.forEach((item) => {
+  for (let i = 0; i < allTheArrays.length; i++) {
+    allTheArrays[i].forEach((item) => {
       if (array1.indexOf(item) !== -1 && array2.indexOf(item) !== -1 && array3.indexOf(item) !== -1 && array4.indexOf(item) !== -1) {
         if (outArr.indexOf(item) === -1) {
           outArr.push(item);
@@ -24,9 +24,11 @@ function commonElements(array1, array2, array3, array4){
       }
     });
   }
-  outArr.length < 1 ? return 'Nothing in Common' : return outArr;
+  if (outArr.length < 1) {
+    return 'Nothing in Common';
+  }
+  return outArr;
 }
 
-console.log(commonElements(array1, array2, array3, array4))
 
 module.exports = commonElements;
