85459c60cff7a163124b7c4b9d8c1c4b928ffb17
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-10-29 10:20:36 -0700

    finished common elements

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -3,17 +3,50 @@
 // duplicates are only counted once;
 // for example == given the following input
 
-  // var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
-  // var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
-  // var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
-  // var array4 = ['ferret',12,12,45,9,66,77,78,2000];
+// var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
+// var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
+// var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
+// var array4 = ['ferret',12,12,45,9,66,77,78,2000];
 
 // your output would be [ 12, 'ferret']
 
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
-function commonElements(array1, array2, array3, array4){
 
+
+function commonElements(...args) {
+	let dictionary = {};
+	let fourElements = [];
+
+	for (let i = 0; i < args.length; i++) {
+		args[i] = getUnique(args[i]);
+		for (let j = 0; j < args[i].length; j++) {
+			if (dictionary[args[i][j]]) {
+				dictionary[args[i][j]] = dictionary[args[i][j]] + 1;
+				if (dictionary[args[i][j]] === 4) {
+					fourElements.push(args[i][j]);
+				}
+			} else {
+				dictionary[args[i][j]] = 1;
+			}
+		}
+	}
+	if (fourElements.length === 0) {
+		return "Nothing in Common!";
+	}
+
+	return fourElements
 }
 
+function getUnique(arr) {
+	var u = {}, a = [];
+	for (var i = 0, l = arr.length; i < l; ++i) {
+		if (u.hasOwnProperty(arr[i])) {
+			continue;
+		}
+		a.push(arr[i]);
+		u[arr[i]] = 1;
+	}
+	return a;
+}
 module.exports = commonElements;
