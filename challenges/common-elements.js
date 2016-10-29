d61e3c51038355546a17ebdd2e27047a87540689
medic27 <jczhang@ucla.edu>
2016-10-29 10:25:12 -0700

    looping and checking if it includes stored values

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -1,3 +1,4 @@
+/* eslint max-len: 0, prefer-rest-params: 0 */
 // given 4 arrays that may contain both numbers and strings
 // return a new array with the numbers and/or strings that appear in all 4 arrays
 // duplicates are only counted once;
@@ -12,8 +13,31 @@
 
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
-function commonElements(array1, array2, array3, array4){
+function commonElements(array1, array2, array3, array4) {
+// loop thru one array and store unique elements in an array
+// compare against elements in next array to find the matching element(s), push these to a new array within this compare loop and
+// eventually update the higher scoped array with these elements
+// continue to do this until all 4 arrays are traversed
+// then return the cache array, unless empty, then return 'nothing in common'
 
+  let commonValues = [];
+  for (let i = 0; i < arguments.length; i++) {
+    // store elements that match commonValues of previous arrays
+    const currentValues = [];
+    const currentArr = arguments[i];
+
+    // loop thru each array
+    for (let j = 0; j < currentArr.length; j++) {
+      if (i === 0 || commonValues.includes(currentArr[j])) {
+        currentValues.push(currentArr[j]);
+      }
+    }
+
+    // update list of common values
+    commonValues = currentValues;
+  }
+
+  return commonValues;
 }
 
 module.exports = commonElements;
