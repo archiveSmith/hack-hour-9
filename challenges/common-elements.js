bc3eaedfe2cc35ad5c1432bc145611852eca0213
William Barnes <barnesw91291@gmail.com>
2016-10-29 10:21:38 -0700

    functional common elements

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -13,7 +13,10 @@
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
 function commonElements(array1, array2, array3, array4){
-
+  return [array1, array2, array3, array4]
+    .reduce((a, b) => a.filter((el) => b.includes(el)))
+    .filter((el, i, array) => array.indexOf(el) === i)
+    .reduce((a, b, i, arr) => arr, 'Nothing in Common!');
 }
 
 module.exports = commonElements;
