e57175d94f7608c0ab2c6b9cfbbfc5b6753e1890
Michael Kulinski <me@michaelkulinski.com>
2016-10-29 09:20:27 -0700

    finding longest array

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -3,17 +3,21 @@
 // duplicates are only counted once;
 // for example == given the following input
 
-  // var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
-  // var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
-  // var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
-  // var array4 = ['ferret',12,12,45,9,66,77,78,2000];
+  var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
+  var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
+  var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
+  var array4 = ['ferret',12,12,45,9,66,77,78,2000];
 
 // your output would be [ 12, 'ferret']
 
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
 function commonElements(array1, array2, array3, array4){
-
+  const longestArray = Math.max(array1.length, array2.length, array3.length, array4.length);
+  console.log(longestArray);
+  return 'Nothing in Common';
 }
+
+console.log(commonElements(array1, array2, array3, array4))
 
 module.exports = commonElements;
