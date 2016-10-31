dcf65e60aa851dd1ebf6ef08694273606c93a74c
Simon Situ <ssitu001@gmail.com>
2016-10-30 22:26:30 -0700

    complete commonElement O(n^2) time :(

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -12,8 +12,25 @@
 
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
-function commonElements(array1, array2, array3, array4){
+function commonElements(...args){
+
+	let sortedArgs = args.sort( (a, b) => b.length - a.length);
+	let intersects = [];
+	
+	for (let i = 0; i < sortedArgs[0].length; i++) {
+		if( sortedArgs[1].includes(sortedArgs[0][i]) && sortedArgs[2].includes(sortedArgs[0][i]) && sortedArgs[3].includes(sortedArgs[0][i]) && !intersects.includes(sortedArgs[0][i])) {
+			intersects.push(sortedArgs[0][i])
+		}
+	}
+
+	return intersects.length ? intersects : "Nothing in Common!";
 
 }
+
+var array1 = [1,4,6,7,'ferrete',12,12,99,2000,'dog','dog',99,1000];
+var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
+var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
+var array4 = ['ferret',12,12,45,9,66,77,78,2000];
+commonElements(array1, array2, array3, array4);
 
 module.exports = commonElements;
