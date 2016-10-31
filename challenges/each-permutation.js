4346eae37dbf00e850f6d248f743c913a6633f25
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-10-31 09:45:10 -0700

    updated permutions solution

--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -17,7 +17,33 @@ eachPermutation([1, 2, 3], function(perm) {
 */
 
 function eachPermutation(arr, callback) {
+  let result = permutator(arr);
+  let returnResult = [];
+  for (let j = 0; j < arr.length; j++) {
+    returnResult.push(callback(result[j]));
+  }
+  return result;
 
+  function permutator(inputArr) {
+    var results = [];
+
+    function permute(arr, memo) {
+      var cur, memo = memo || [];
+
+      for (var i = 0; i < arr.length; i++) {
+        cur = arr.splice(i, 1);
+        if (arr.length === 0) {
+          results.push(memo.concat(cur));
+        }
+        permute(arr.slice(), memo.concat(cur));
+        arr.splice(i, 0, cur[0]);
+      }
+
+      return results;
+    }
+
+    return permute(inputArr);
+  }
 }
 
 
