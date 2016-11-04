76c12b4b4a9ed836be5860346c9ac3cb3996b86c
bryanyee <yee.bryan.c@gmail.com>
2016-11-04 11:34:47 -0700

    Complete each-permutation

--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -15,11 +15,34 @@ eachPermutation([1, 2, 3], function(perm) {
 [ 2, 3, 1 ]
 [ 3, 1, 2 ]
 [ 3, 2, 1 ]
+
+2 => 2
+3 => 6
+4 => 24
 */
 
 function eachPermutation(arr, callback) {
+  const permutations = getPermutations(arr);
 
+  permutations.forEach((el) => callback(el));
 }
 
+function getPermutations(array) {
+  if (array.length === 1) return array;
+
+  const permutations = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const tempArr = array.slice(0, i).concat(array.slice(i + 1, array.length));
+    const prev = getPermutations(tempArr);
+    const curr = [array[i]];
+
+    prev.forEach((el) => {
+      permutations.push(curr.concat(el));
+    });
+  }
+
+  return permutations;
+}
 
 module.exports = eachPermutation;
