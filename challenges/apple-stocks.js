79a8731a4a4a80a6da1e67b3f72fbfc46354bdd2
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-11-06 15:22:45 -0800

    more changes to apple stocks

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -12,25 +12,25 @@
  *  Return 0 if no profit is possible OR if input is invalid.
  */
 
-// function bestProfit(stock_prices_yesterday){
-// 	if(Array.isArray(stock_prices_yesterday)) {
-// 		let maxDiff = 0;
-// 		let currentDiff = 0;
-// 		let length = stock_prices_yesterday.length;
-// 		for (let i = 0; i < length; i++){
-// 			for (let j = i ; j < length ; j++){
-// 				currentDiff = stock_prices_yesterday[j] - stock_prices_yesterday[i];
-// 				if (currentDiff > maxDiff){
-// 					maxDiff = currentDiff;
-// 				}
-// 			}
-// 		}
-// 		return maxDiff;
-// 	}
-// 	else return 0;
-// }
+function bestProfit(stock_prices_yesterday){
+	if(Array.isArray(stock_prices_yesterday)) {
+		let maxDiff = 0;
+		let currentDiff = 0;
+		let length = stock_prices_yesterday.length;
+		for (let i = 0; i < length; i++){
+			for (let j = i ; j < length ; j++){
+				currentDiff = stock_prices_yesterday[j] - stock_prices_yesterday[i];
+				if (currentDiff > maxDiff){
+					maxDiff = currentDiff;
+				}
+			}
+		}
+		return maxDiff;
+	}
+	else return 0;
+}
 
-function bestProfit(stock_prices_yesterday) {
+function bestProfit2(stock_prices_yesterday) {
     var low = stock_prices_yesterday[0];
 
     return stock_prices_yesterday.reduce((maxProfit, currStock) => {
