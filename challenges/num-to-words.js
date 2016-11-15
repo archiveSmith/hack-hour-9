e6d054fdc8a0f9be7d65eadd38e0829f49ff657e
yantonsoup <ygoldobin@gmail.com>
2016-11-15 12:07:09 -0800

    quick commit

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -29,6 +29,7 @@ function numToWords(num) {
 	console.log("numThousands ", numThousands)
 	
 	resultString = '';
+	
 	smallMatch= {0:"Zero", 1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five",6:"Six", 7:"Seven", 8:"Eight", 9:"Nine", 10:"Ten", 11:"Eleven",12:"Twelve",13:"Thirteen",14:"Fourteen",15:"Fifteen",16:"Sixteen",17:"Seventeen",18:"Eighteen",19:"Nineteen"};
 	
 	console.log(smallMatch[num])
