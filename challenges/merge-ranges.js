8d7ed5fca583fbc8972772840282a20a426c7d3b
Michael Kulinski <me@michaelkulinski.com>
2016-11-16 16:05:07 -0800

    trying it

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -15,28 +15,19 @@
 // for each array check the first number compared to all the other numbers
 
 function mergeRanges(array) {
-  let sortedArr = array;
-  let newArr = [];
+  const sortedArr = array.sort((a, b) => a - b);
+  const outArr = [array[0]];
 
-  sortedArr.sort((item1, item2) => {
-    return item1[0] - item2[0];
-  })
-
-  for (let i = 0; i < sortedArr.length - 1; i++) {
-    if (sortedArr[i][1] >= sortedArr[i + 1][0]) {
-      let tempArr = [sortedArr[i][0], sortedArr[i + 1][1]];
-      newArr.push(tempArr);
+  for (let i = 1; i < sortedArr.length - 1; i++) {
+    if (sortedArr[i - 1][1] > sortedArr[i][0]) {
+      const outNum = Math.max(sortedArr[i - 1][1], sortedArr[i][1]);
+      const tempArr = [].concat(sortedArr[i - 1][0], outNum);
+      outArr[i - 1] = tempArr;
     } else {
-      newArr.push(sortedArr[i]);
+      outArr.push(sortedArr[i]);
     }
   }
-  return sortedArr;
-
+  return outArr;
 }
-
-const times = [[3, 5], [0, 1], [4, 8], [10, 12], [9, 10]];
-console.log(times)
-
-console.log(mergeRanges(times));
 
 module.exports = mergeRanges;
