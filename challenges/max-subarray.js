648a6da19e17b9fe7050d988589dfcfbd3347b60
Dana Tran <dana.l.tran@gmail.com>
2016-11-28 13:57:21 -0800

    redo max subarray, trying to get array of length 1 test to pass

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -54,7 +54,11 @@
 function maxSubarray(arr) {
 
   if (arr.length === 1) {
-    return arr[0];
+    if (arr[0] > 0){
+      return arr[0];
+    } else {
+      return 0;
+    }
   }
 
   var atLeastOnePositive = false;
