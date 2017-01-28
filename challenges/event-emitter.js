36aa2ec52f99f3d32261e833c1226ffef3a24f9f
sneakykate <sneakyk8@gmail.com>
2017-01-28 13:16:45 -0800

    check answer eventemitter

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -35,3 +35,24 @@ function EventEmitter() {
 }
 
 module.exports = EventEmitter;
+
+// Above passes tests, below is official solution
+
+//function EventEmitter() {
+// }
+
+// EventEmitter.prototype.on = function(funcName, func) {
+//   if (!this[funcName]) {
+//     this[funcName] = [];
+//   }
+//   this[funcName].push(func);
+// };
+
+// EventEmitter.prototype.trigger = function(funcName) {
+//   var args = Array.prototype.slice.call(arguments, 1);
+//   if (this[funcName]) {
+//     for (var i = 0; i < this[funcName].length; i++) {
+//       this[funcName][i].apply(this, args);
+//     }
+//   }
+// };
